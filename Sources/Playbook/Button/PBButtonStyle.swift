7bd357239687a4f90620e72808b1fc284e6d7f90
gavhuang <47684670+gavhuang@users.noreply.github.com>
2023-03-15 14:38:32 -0400

    Format document

--- Sources/Playbook/Button/PBButtonStyle.swift
+++ Sources/Playbook/Button/PBButtonStyle.swift
@@ -8,19 +8,19 @@
 import SwiftUI
 
 public struct PBButtonStyle: ButtonStyle {
-    var variant: PBButtonVariant = .primary
-    var size: PBButtonSize = .medium
-    var disabled: Bool = false
+  var variant: PBButtonVariant = .primary
+  var size: PBButtonSize = .medium
+  var disabled: Bool = false
 
-    public init(
-        variant: PBButtonVariant? = .primary,
-        size: PBButtonSize? = .medium,
-        disabled: Bool? = false
-    ) {
-        self.variant = variant ?? self.variant
-        self.disabled = disabled ?? self.disabled
-        self.size = size ?? self.size
-    }
+  public init(
+    variant: PBButtonVariant? = .primary,
+    size: PBButtonSize? = .medium,
+    disabled: Bool? = false
+  ) {
+    self.variant = variant ?? self.variant
+    self.disabled = disabled ?? self.disabled
+    self.size = size ?? self.size
+  }
 
   public func makeBody(configuration: Configuration) -> some View {
     configuration.label
@@ -49,93 +49,93 @@ public struct PBButtonStyle: ButtonStyle {
 }
 
 public enum PBButtonVariant {
-    case primary
-    case secondary
-    case link
+  case primary
+  case secondary
+  case link
 
-    func foregroundColor(_ disabled: Bool) -> Color {
-        if disabled { return Color.pbTextDefault.opacity(0.5) }
+  func foregroundColor(_ disabled: Bool) -> Color {
+    if disabled { return Color.pbTextDefault.opacity(0.5) }
 
-        switch self {
-        case .primary: return .white
-        case .secondary: return .pbPrimary
-        case .link: return .pbPrimary
-        }
+    switch self {
+    case .primary: return .white
+    case .secondary: return .pbPrimary
+    case .link: return .pbPrimary
     }
+  }
 
-    func backgroundColor(_ disabled: Bool) -> Color {
-        switch (self, disabled) {
-        case (.primary, true): return Color.pbNeutral.opacity(0.4)
-        case (.primary, false): return .pbPrimary
-        case (.secondary, _): return Color.pbPrimary.opacity(0.05)
-        case (.link, _): return .clear
-        }
+  func backgroundColor(_ disabled: Bool) -> Color {
+    switch (self, disabled) {
+    case (.primary, true): return Color.pbNeutral.opacity(0.4)
+    case (.primary, false): return .pbPrimary
+    case (.secondary, _): return Color.pbPrimary.opacity(0.05)
+    case (.link, _): return .clear
     }
+  }
 }
 
 public enum PBButtonSize {
-    case small
-    case medium
-    case large
+  case small
+  case medium
+  case large
 
-    public var fontSize: CGFloat {
-        switch self {
-        case .small:
-            return 12
-        case .medium:
-            return 14
-        case .large:
-            return 18
-        }
+  public var fontSize: CGFloat {
+    switch self {
+    case .small:
+      return 12
+    case .medium:
+      return 14
+    case .large:
+      return 18
     }
+  }
 
-    func verticalPadding() -> CGFloat {
-        return fontSize / 2
-    }
+  func verticalPadding() -> CGFloat {
+    return fontSize / 2
+  }
 
-    func horizontalPadding() -> CGFloat {
-        return fontSize * 2.42
-    }
+  func horizontalPadding() -> CGFloat {
+    return fontSize * 2.42
+  }
 
-    func minHeight() -> CGFloat {
-        return self == .small ? 36 : 40
-    }
+  func minHeight() -> CGFloat {
+    return self == .small ? 36 : 40
+  }
 }
 
 struct PBButtonStyle_Previews: PreviewProvider {
-    static var previews: some View {
-        registerFonts()
+  static var previews: some View {
+    registerFonts()
 
-        return Group {
-            VStack {
-                Button("Button Primary") {}
-                    .buttonStyle(PBButtonStyle())
-                Button("Button Primary Disabled") {}
-                    .buttonStyle(PBButtonStyle(disabled: true))
-                Button("Button Secondary") {}
-                    .buttonStyle(PBButtonStyle(variant: .secondary))
-                Button("Button Secondary Disabled") {}
-                    .buttonStyle(PBButtonStyle(variant: .secondary, disabled: true))
-                Button("Button Link") {}
-                    .buttonStyle(PBButtonStyle(variant: .link))
-                Button("Button Link Disabled") {}
-                    .buttonStyle(PBButtonStyle(variant: .link, disabled: true))
+    return Group {
+      VStack {
+        Button("Button Primary") {}
+          .buttonStyle(PBButtonStyle())
+        Button("Button Primary Disabled") {}
+          .buttonStyle(PBButtonStyle(disabled: true))
+        Button("Button Secondary") {}
+          .buttonStyle(PBButtonStyle(variant: .secondary))
+        Button("Button Secondary Disabled") {}
+          .buttonStyle(PBButtonStyle(variant: .secondary, disabled: true))
+        Button("Button Link") {}
+          .buttonStyle(PBButtonStyle(variant: .link))
+        Button("Button Link Disabled") {}
+          .buttonStyle(PBButtonStyle(variant: .link, disabled: true))
 
-                HStack {
-                    Button("Cancel") {}
-                        .buttonStyle(PBButtonStyle(variant: .secondary, size: .small))
+        HStack {
+          Button("Cancel") {}
+            .buttonStyle(PBButtonStyle(variant: .secondary, size: .small))
 
-                    Button("Save") {}
-                        .buttonStyle(PBButtonStyle(variant: .primary, size: .small))
-                }
-
-                Button("Button Primary Medium") {}
-                    .buttonStyle(PBButtonStyle(variant: .primary, size: .medium))
-
-                Button("Button Primary Large") {}
-                    .buttonStyle(PBButtonStyle(variant: .primary, size: .large))
-            }
-            .padding(.horizontal, 20)
+          Button("Save") {}
+            .buttonStyle(PBButtonStyle(variant: .primary, size: .small))
         }
+
+        Button("Button Primary Medium") {}
+          .buttonStyle(PBButtonStyle(variant: .primary, size: .medium))
+
+        Button("Button Primary Large") {}
+          .buttonStyle(PBButtonStyle(variant: .primary, size: .large))
+      }
+      .padding(.horizontal, 20)
     }
+  }
 }
