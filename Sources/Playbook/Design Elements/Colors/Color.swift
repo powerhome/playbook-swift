5b9bf17a8caad5b37132329eac98808be4f54830
Isis Silva <>
2023-04-18 12:25:52 -0400

    Fix color assets

--- Sources/Playbook/Design Elements/Colors/Color.swift
+++ Sources/Playbook/Design Elements/Colors/Color.swift
@@ -1,5 +1,5 @@
 //
-//  Color.swift
+//  Colors.swift
 //
 //
 //  Created by Lucas C. Feijo on 12/07/21.
@@ -8,229 +8,163 @@
 import SwiftUI
 
 public extension Color {
-  // MARK: RGB Colors
+  static let pbPrimary = Color("Primary", bundle: .module)
+  static let card = Color("Card", bundle: .module)
+  static let active = Color("Active", bundle: .module)
+  static let border = Color("Border", bundle: .module)
+  static let shadow = Color("Shadow", bundle: .module)
+  static let hover = Color("Hover", bundle: .module)
 
-  static let pbGreen = Color("Green", bundle: .module)
-  static let pbPurple = Color("Purple", bundle: .module)
-  static let pbRed = Color("Red", bundle: .module)
-  static let pbRoyal = Color("Royal", bundle: .module)
-  static let pbTeal = Color("Teal", bundle: .module)
-  static let pbYellow = Color("Yellow", bundle: .module)
-  static let pbOrange = Color("Orange", bundle: .module)
-  static let pbDefault = Color("Default", bundle: .module)
+  var subtle: Color {
+    self.opacity(0.1)
+  }
 
-  // MARK: Text Colors
+  static func text(_ variant: TextColor) -> Color {
+    switch variant {
+    case .textDefault: return Color("TextDefault", bundle: .module)
+    case .light: return Color("TextLight", bundle: .module)
+    case .lighter: return Color("TextLighter", bundle: .module)
+    case .white: return Color.white
+    }
+  }
 
-  static let pbTextDefault = Color("TextDefault", bundle: .module)
-  /** light: #687887  dark: #FFFFFF 0.6 */
-  static let pbTextLight = Color("TextLight", bundle: .module)
-  static let pbTextLighter = Color("TextLighter", bundle: .module)
+  static func background(_ variant: BackgroundColor) -> Color {
+    switch variant {
+    case .light: return Color("BackgroundLight", bundle: .module)
+    case .dark: return Color("BackgroundDark", bundle: .module)
+    case .`default`: return Color("Background", bundle: .module)
+    }
+  }
 
-  // MARK: Backgrounds
+  static func status(_ variant: StatusColor, subtle: Bool = false) -> Color {
+    switch variant {
+    case .success: return Color("Success", bundle: .module).opacity(subtle ? 0.1 : 1)
+    case .warning: return Color("Warning", bundle: .module).opacity(subtle ? 0.1 : 1)
+    case .error: return Color("Error", bundle: .module).opacity(subtle ? 0.1 : 1)
+    case .info: return Color("Info", bundle: .module).opacity(subtle ? 0.1 : 1)
+    case .neutral: return Color("Neutral", bundle: .module).opacity(subtle ? 0.1 : 1)
+    case .primary: return Color("Primary", bundle: .module)
+    }
+  }
 
-  static let pbBackground = Color("Background", bundle: .module)
-  /** light: #F3F7FB  dark: #0A0527 */
-  static let pbBackgroundLight = Color("BackgroundLight", bundle: .module)
-  static let pbNavigationPrimary = Color("NavigationPrimary", bundle: .module)
-  static let pbNavigationSecondary = Color("NavigationSecondary", bundle: .module)
+  static func data(_ variant: DataColor) -> Color {
+    switch variant {
+    case .data1: return Color("Data1", bundle: .module)
+    case .data2: return Color("Data2", bundle: .module)
+    case .data3: return Color("Data3", bundle: .module)
+    case .data4: return Color("Data4", bundle: .module)
+    case .data5: return Color("Data5", bundle: .module)
+    case .data6: return Color("Data6", bundle: .module)
+    case .data7: return Color("Data7", bundle: .module)
+    case .data8: return Color("Data8", bundle: .module)
+    }
+  }
 
-  // MARK: Cards
+  static func product(_ variant: ProductColor, category: ProductColor.Category) -> Color {
+    
+    let productCategory = category == .background ? "Background" : "Highlight"
+    switch variant {
+    case .product1: return Color("Product1" + productCategory, bundle: .module)
+    case .product2: return Color("Product2" + productCategory, bundle: .module)
+    case .product3: return Color("Product3" + productCategory, bundle: .module)
+    case .product4: return Color("Product4" + productCategory, bundle: .module)
+    case .product5: return Color("Product5" + productCategory, bundle: .module)
+    case .product6: return Color("Product6" + productCategory, bundle: .module)
+    case .product7: return Color("Product7" + productCategory, bundle: .module)
+    case .product8: return Color("Product8" + productCategory, bundle: .module)
+    case .product9: return Color("Product9" + productCategory, bundle: .module)
+    case .product10: return Color("Product10" + productCategory, bundle: .module)
+    }
+    
+  }
 
-  static let pbCard = Color("Card", bundle: .module)
-
-  // MARK: Status
-
-  static let pbSuccess = Color("Green", bundle: .module)
-  static let pbWarning = Color("Yellow", bundle: .module)
-  /** Status Error light: #FF2229  dark: #FF4A50 */
-  static let pbError = Color("Error", bundle: .module)
-  static let pbInfo = Color("Teal", bundle: .module)
-  static let pbNeutral = Color("Slate", bundle: .module)
-  // also uses pbPrimary ("Royal")
-  /** Status Secondary Colors */
-  /** #00FD91 */
-  static let pbSuccessSecondary = Color("GreenLighten10", bundle: .module)
-  /** #FFCB2D */
-  static let pbWarningSecondary = Color("YellowLighten10", bundle: .module)
-  /** #FF4F55 */
-  static let pbErrorDarkBody = Color("ErrorDarkBody", bundle: .module)
-  /** #FF555A */
-  static let pbErrorSecondary = Color("RedLighten10", bundle: .module)
-  /** #0BE9FF */
-  static let pbInfoSecondary = Color("TealLighten10", bundle: .module)
-  /** #E0E6EA */
-  static let pbNeutralSecondary = Color("SlateLighten10", bundle: .module)
-  /** #036CFF */
-  static let pbPrimarySecondary = Color("RoyalLighten10", bundle: .module)
-
-  // MARK: Status Color Text
-
-  static let pbSuccessText = Color("Green", bundle: .module)
-  static let pbErrorText = Color("Red", bundle: .module) /** Red for dark and light modes */
-  static let pbInfoText = Color("Teal", bundle: .module)
-  static let pbPrimaryText = Color("Royal", bundle: .module)
-  /** #C69500 */
-  static let pbWarningText = Color("YellowDarken10", bundle: .module)
-  /** #93A8B8 */
-  static let pbNeutralText = Color("SlateDarken15", bundle: .module)
-
-  // MARK: Actions
-
-  /** light: #0056CF  dark: #0055CF */
-  static let pbPrimary = Color("Royal", bundle: .module)
-  static let pbSecondary = Color("Yellow", bundle: .module)
-  static let pbTertiary = Color("Purple", bundle: .module)
-  /** same color as primary (royal) */
-  static let pbPrimaryAction = Color("Royal", bundle: .module)
-
-  // MARK: Mentions
-
-  static let pbMention = Color("Mention", bundle: .module)
-  static let pbMentionMe = Color("MentionMe", bundle: .module)
-  static let pbMentionText = Color("MentionText", bundle: .module)
-
-  // MARK: Active
-
-  static let pbActive = Color("Active", bundle: .module)
-
-  // MARK: Border
-
-  /** light: #E4E8F0   dark #E3E8EF 23% : */
-  static let pbBorder = Color("Border", bundle: .module)
-
-  // MARK: Shadow
-
-  static let pbShadow = Color("Shadow", bundle: .module)
-  static let pbHover = Color("Hover", bundle: .module)
-
-  // MARK: Product Colors
-
-  static let pbWindows = Color("Royal", bundle: .module)
-  static let pbSiding = Color("Yellow", bundle: .module)
-  static let pbDoors = Color("Teal", bundle: .module)
-  static let pbSolar = Color("Green", bundle: .module)
-  static let pbRoofing = Color("Slate", bundle: .module)
-  static let pbGutters = Color("Purple", bundle: .module)
-  static let pbInsulation = Color("Red", bundle: .module)
-
-  // MARK: Category Colors
-
-  static let pbCategoryOne = Color("Royal", bundle: .module)
-  static let pbCategoryTwo = Color("Category2", bundle: .module)
-  static let pbCategoryThree = Color("Yellow", bundle: .module)
-  static let pbCategoryFour = Color("Category4", bundle: .module)
-  static let pbCategoryFive = Color("Category5", bundle: .module)
-  static let pbCategorySix = Color("Category6", bundle: .module)
-  static let pbCategorySeven = Color("Category7", bundle: .module)
-  static let pbCategoryEight = Color("Category8", bundle: .module)
-  static let pbCategoryNine = Color("Category9", bundle: .module)
-  static let pbCategoryTen = Color("Category10", bundle: .module)
-  static let pbCategoryEleven = Color("Red", bundle: .module)
-  static let pbCategoryTwelve = Color("Category12", bundle: .module)
-  static let pbCategoryThirteen = Color("Category13", bundle: .module)
-  static let pbCategoryFourteen = Color("Category14", bundle: .module)
-  static let pbCategoryFifteen = Color("Category15", bundle: .module)
-  static let pbCategorySixteen = Color("Category16", bundle: .module)
-  static let pbCategorySeventeen = Color("Category17", bundle: .module)
-  static let pbCategoryEighteen = Color("Category18", bundle: .module)
-  static let pbCategoryNineteen = Color("Category19", bundle: .module)
-  static let pbCategoryTwenty = Color("Category20", bundle: .module)
-  static let pbCategoryTwentyOne = Color("Category21", bundle: .module)
-
-  // MARK: Data Colors
-
-  static let pbDataOne = Color("Royal", bundle: .module)
-  static let pbDataTwo = Color("Yellow", bundle: .module)
-  static let pbDataThree = Color("Purple", bundle: .module)
-  static let pbDataFour = Color("Green", bundle: .module)
-  static let pbDataFive = Color("Orange", bundle: .module)
-  /** #144075 */
-  static let pbDataSix = Color("Data6", bundle: .module)
-  static let pbDataSeven = Color("Teal", bundle: .module)
-  static let pbDataEight = Color("Red", bundle: .module)
-
-  // MARK: Focus Colors
-
-  /** light: #EEFBFF opacity 50%  Dark: #144075 opacity 50%    */
-  static let pbFocusColor = Color("Focus", bundle: .module)
+  static func category(_ variant: CategoryColor) -> Color {
+    switch variant {
+    case .category1: return Color("Category1", bundle: .module)
+    case .category2: return Color("Category2", bundle: .module)
+    case .category3: return Color("Category3", bundle: .module)
+    case .category4: return Color("Category4", bundle: .module)
+    case .category5: return Color("Category5", bundle: .module)
+    case .category6: return Color("Category6", bundle: .module)
+    case .category7: return Color("Category7", bundle: .module)
+    case .category8: return Color("Category8", bundle: .module)
+    case .category9: return Color("Category9", bundle: .module)
+    case .category10: return Color("Category10", bundle: .module)
+    case .category11: return Color("Category11", bundle: .module)
+    case .category12: return Color("Category12", bundle: .module)
+    case .category13: return Color("Category13", bundle: .module)
+    case .category14: return Color("Category14", bundle: .module)
+    case .category15: return Color("Category15", bundle: .module)
+    case .category16: return Color("Category16", bundle: .module)
+    case .category17: return Color("Category17", bundle: .module)
+    case .category18: return Color("Category18", bundle: .module)
+    case .category19: return Color("Category19", bundle: .module)
+    case .category20: return Color("Category20", bundle: .module)
+    case .category21: return Color("Category21", bundle: .module)
+    }
+  }
 }
 
 public extension Color {
-  // MARK: Grouped Colors
+  enum TextColor: String, CaseIterable {
+    case textDefault, light, lighter, white
+  }
 
-  static let pbCategoryColors: [Color] = [
-    .pbCategoryOne,
-    .pbCategoryTwo,
-    .pbCategoryThree,
-    .pbCategoryFour,
-    .pbCategoryFive,
-    .pbCategorySix,
-    .pbCategorySeven,
-    .pbCategoryEight,
-    .pbCategoryNine,
-    .pbCategoryTen,
-    .pbCategoryEleven,
-    .pbCategoryTwelve,
-    .pbCategoryThirteen,
-    .pbCategoryFourteen,
-    .pbCategoryFifteen,
-    .pbCategorySixteen,
-    .pbCategorySeventeen,
-    .pbCategoryEighteen,
-    .pbCategoryNineteen,
-    .pbCategoryTwenty,
-    .pbCategoryTwentyOne
-  ]
+  enum BackgroundColor: String, CaseIterable {
+    case `default`, light, dark
+  }
 
-  static let pbProductColors: [Color] = [
-    .pbWindows,
-    .pbSiding,
-    .pbDoors,
-    .pbSolar,
-    .pbRoofing,
-    .pbGutters,
-    .pbInsulation
-  ]
+  enum StatusColor: String, CaseIterable {
+    case success, warning, error, info, neutral, primary
+  }
 
-  static let pbStatusColors: [Color] = [
-    .pbSuccess,
-    .pbWarning,
-    .pbError,
-    .pbInfo,
-    .pbNeutral,
-    .pbSuccessSecondary,
-    .pbWarningSecondary,
-    .pbErrorSecondary,
-    .pbErrorDarkBody,
-    .pbInfoSecondary,
-    .pbPrimarySecondary,
-    .pbNeutralSecondary,
-    .pbPrimary
-  ]
+  enum DataColor: String, CaseIterable {
+    case data1, data2, data3, data4, data5, data6, data7, data8
+  }
 
-  static let pbBackgroundColors: [Color] = [
-    .pbBackground,
-    .pbNavigationPrimary,
-    .pbNavigationSecondary
-  ]
+  enum ProductColor: String, CaseIterable {
+    case product1
+    case product2
+    case product3
+    case product4
+    case product5
+    case product6
+    case product7
+    case product8
+    case product9
+    case product10
+    
+    public enum Category {
+      case highlight, background
+    }
+  }
 
-  static let pbTextColors: [Color] = [
-    .pbTextDefault,
-    .pbTextLight,
-    .pbTextLighter
-  ]
-
-  static let pbStatusColorText: [Color] = [
-    .pbSuccessText,
-    .pbWarningText,
-    .pbErrorText,
-    .pbInfoText,
-    .pbNeutralText,
-    .pbPrimaryText
-  ]
+  enum CategoryColor {
+    case category1
+    case category2
+    case category3
+    case category4
+    case category5
+    case category6
+    case category7
+    case category8
+    case category9
+    case category10
+    case category11
+    case category12
+    case category13
+    case category14
+    case category15
+    case category16
+    case category17
+    case category18
+    case category19
+    case category20
+    case category21
+  }
 }
 
-extension Color {
+public extension Color {
   init(hex: String) {
     let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
     var int: UInt64 = 0
@@ -256,13 +190,12 @@ extension Color {
     )
   }
 
-  /// Example: Color(hex: 0x000000, alpha: 0.2)
   init(hex: UInt, alpha: Double = 1) {
     self.init(
       .sRGB,
-      red: Double((hex >> 16) & 0xFF) / 255,
-      green: Double((hex >> 08) & 0xFF) / 255,
-      blue: Double((hex >> 00) & 0xFF) / 255,
+      red: Double((hex >> 16) & 0xff) / 255,
+      green: Double((hex >> 08) & 0xff) / 255,
+      blue: Double((hex >> 00) & 0xff) / 255,
       opacity: alpha
     )
   }
