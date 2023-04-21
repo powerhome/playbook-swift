3ece8eb32ebe9006abf1e6e9eb09c55e05bdf877
Isis Silva <>
2023-04-21 10:20:11 -0400

    Solve  package conflicts

--- Package.swift
+++ Package.swift
@@ -16,8 +16,12 @@ let package = Package(
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    
-
+    .package(url: "https://github.com/realm/SwiftLint.git", from: "0.51.0"),
+    .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.51.7"),
+    .package(
+      url: "https://github.com/pointfreeco/swift-snapshot-testing",
+      from: "1.11.0"
+    ),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,8 +29,15 @@ let package = Package(
     .target(
       name: "Playbook",
       dependencies: [],
-      resources: [.process("Resources/Assets/")]
+      resources: [.process("Resources/Assets/")],
+      plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
     ),
-   
+    .testTarget(
+      name: "SnapshotTests",
+      dependencies: [
+        "Playbook",
+        .product(name: "SnapshotTesting", package: "swift-snapshot-testing"),
+      ]
+    ),
   ]
 )
