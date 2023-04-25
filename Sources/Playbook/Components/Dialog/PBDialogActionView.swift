815d76eb477362bfadcb461b1975f4138afeb2df
Ísis <isisms2602@gmail.com>
2023-04-25 10:41:13 -0400

    [PBIOS-13] Validation PBDialog (#51)
    ## Summary
    - Updated PB Dialog kit to be consistent with the web version
    
    ## Additional Details
    - [Runway
    Story](https://nitro.powerhrg.com/runway/backlog_items/PBIOS-13)
    - Refactored PB Dialog kit to conform to Playbook Swift standards
    
    ## Screenshots 
    
    ### Simple
    | Before | After |
    | --------------------------------- | ---------------------------------
    |
    | ![Screen Shot 2023-04-05 at 15 44
    58](https://user-images.githubusercontent.com/60269827/230175843-0ca72e18-d186-46d4-ac70-abf781f93bc7.png)
    | ![Screen Shot 2023-04-05 at 15 31
    28](https://user-images.githubusercontent.com/60269827/230173581-8a878959-8bc7-4221-a523-c6bc894c5e51.png)
    |
    
    ### Complex
    | Before | After |
    | --------------------------------- | ---------------------------------
    |
    | ![Screen Shot 2023-04-05 at 15 45
    12](https://user-images.githubusercontent.com/60269827/230175966-dc097c4d-7f3e-4722-91f7-6b9a4855122a.png)
    | ![Screen Shot 2023-04-05 at 15 31
    39](https://user-images.githubusercontent.com/60269827/230174139-e5a52be4-6da4-47ae-9a14-3f3faa0ba4df.png)
    |
    
    
    
    ### Size
    | Before | After |
    | --------------------------------- | ---------------------------------
    |
    | ![Screen Shot 2023-04-05 at 15 45
    41](https://user-images.githubusercontent.com/60269827/230176308-4f2c83f9-8d59-4fc3-b110-67b3a009ba05.png)
    | ![Screen Shot 2023-04-05 at 15 33
    46](https://user-images.githubusercontent.com/60269827/230174253-88a62b9f-2331-429f-b4e4-837a85df23e4.png)
    |
    
    ### Stacked
    | Before | After |
    | --------------------------------- | ---------------------------------
    |
    | N/A | ![Screen Shot 2023-04-05 at 15 34
    00](https://user-images.githubusercontent.com/60269827/230174318-846ab1e8-e699-4475-b00f-4bd2874bf4c0.png)
    |
    
    ### Status
    | Before | After |
    | --------------------------------- | ---------------------------------
    |
    | N/A | ![Screen Shot 2023-04-05 at 15 34
    11](https://user-images.githubusercontent.com/60269827/230174404-4ecb5ec0-88f0-4b13-8724-da866b8dd8f1.png)
    |
    
    ## Breaking Changes
    No
    
    ## Observations
    - Tested the updated PB Dialog kit in a demo application
    - Validated the Colors story to enforce proper colors
    
    ## Checklist
    
    - [x] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, `enhancement`, or `improvement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [x] **SCREENSHOTS** - Please add a screenshot or two. For UI changes,
    you MUST provide before and after screenshots.
    - [x] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [x] **TESTING** - Have you tested your story?

---
+++ Sources/Playbook/Components/Dialog/PBDialogActionView.swift
@@ -0,0 +1,93 @@
+//
+//  PBDialogActionView.swift
+//  
+//
+//  Created by Isis Silva on 03/04/23.
+//
+
+import SwiftUI
+
+struct PBDialogActionView: View {
+  let isStacked: Bool
+  let confirmButton: (String, (() -> Void))?
+  let confirmButtonStyle: PBButtonStyle
+  let cancelButton: (String, (() -> Void)?)?
+  let cancelButtonStyle: PBButtonStyle
+
+  public init(
+    isStacked: Bool = false,
+    confirmButton: (String, (() -> Void))? = nil,
+    confirmButtonStyle: PBButtonStyle = PBButtonStyle(variant: .primary),
+    cancelButton: (String, (() -> Void)?)? = nil,
+    cancelButtonStyle: PBButtonStyle = PBButtonStyle(variant: .link)
+  ) {
+    self.isStacked = isStacked
+    self.confirmButton = confirmButton
+    self.confirmButtonStyle = confirmButtonStyle
+    self.cancelButton = cancelButton
+    self.cancelButtonStyle = cancelButtonStyle
+  }
+
+  var body: some View {
+    AdaptiveStack(isStacked: isStacked) {
+      if let confirmButton = confirmButton {
+        Button {
+          confirmButton.1()
+        } label: {
+          stackedLabel(confirmButton.0)
+        }
+        .buttonStyle(confirmButtonStyle)
+      }
+
+      if let cancelButton = cancelButton {
+        if !isStacked {
+          Spacer()
+        }
+        Button {
+          (cancelButton.1 ?? {})()
+        } label: {
+          stackedLabel(cancelButton.0)
+        }
+        .buttonStyle(cancelButtonStyle)
+        .padding(.top, -8)
+      }
+    }
+    .frame(maxWidth: .infinity)
+  }
+
+  @ViewBuilder
+  func stackedLabel(_ text: String) -> some View {
+    if isStacked {
+      Text(text).frame(maxWidth: .infinity)
+    } else {
+      Text(text)
+    }
+  }
+}
+
+struct PBDialogActionView_Previews: PreviewProvider {
+  static var previews: some View {
+    registerFonts()
+    return List {
+      PBDialogActionView(
+        confirmButton: ("Okay", {}),
+        confirmButtonStyle: PBButtonStyle(variant: .primary),
+        cancelButton: ("Cancel", {}),
+        cancelButtonStyle: PBButtonStyle(variant: .link)
+      )
+
+      PBDialogActionView(
+        isStacked: true,
+        confirmButton: ("Yes, Action", {}),
+        confirmButtonStyle: PBButtonStyle(variant: .primary),
+        cancelButton: ("No, Cancel", {}),
+        cancelButtonStyle: PBButtonStyle(variant: .secondary)
+      )
+
+      PBDialogActionView(
+        confirmButton: ("Okay", {}),
+        confirmButtonStyle: PBButtonStyle(variant: .primary)
+      )
+    }
+  }
+}
