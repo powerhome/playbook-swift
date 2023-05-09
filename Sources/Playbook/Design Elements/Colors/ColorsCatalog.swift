4655d9fe5df4da420e4227dd95155f244d5f2381
Ísis <isisms2602@gmail.com>
2023-05-09 15:04:35 -0300

    [PBIOS-74] Playbook Development test 3.0.0 (#68)
    ## Summary
    - Fixes for Playbook 3.0.0 release
    
    ## Additional Details
    - [[Runway
    Story]](https://nitro.powerhrg.com/runway/backlog_items/ROGUE-757)
    - Fix dialog for macOS
    - Fix hover color
    - Fix buttons style accessibility level
    
    ## Breaking Changes
    Yes
    
    ## Testing
    Tests were preview performed by connect ninjas.
    
    ## Checklist
    
    - [x] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [x] **SCREENSHOTS** - Please add a screenshot or two. For UI changes,
    you MUST provide before and after screenshots.
    - [x] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [x] **TESTING** - Have you tested your story?

--- Sources/Playbook/Design Elements/Colors/ColorsCatalog.swift
+++ Sources/Playbook/Design Elements/Colors/ColorsCatalog.swift
@@ -7,15 +7,10 @@
 
 import SwiftUI
 
-#if DEBUG
 struct ColorsCatalog: View {
   var body: some View {
     let shape = Circle().frame(width: 60).pbShadow(.deep)
     List {
-      Section("Main") {
-        shape.foregroundColor(.pbPrimary)
-      }
-
       Section("Text") {
         LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
           ForEach(Color.TextColor.allCases, id: \.self) { color in
@@ -38,49 +33,12 @@ struct ColorsCatalog: View {
         }
       }
 
-      Section("Cards") {
-        shape.foregroundColor(.card)
-      }
-
       Section("Status") {
         LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
           ForEach(Color.StatusColor.allCases, id: \.self) { color in
             VStack {
               shape.foregroundColor(.status(color))
               Text(color.rawValue).pbFont(.caption, color: .text(.light))
-            }
-          }
-        }
-      }
-
-      Section("Status Subtle") {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-          ForEach(Color.StatusColor.allCases, id: \.self) { color in
-            VStack {
-              shape.foregroundColor(Color.status(color, subtle: true))
-              Text(color.rawValue).pbFont(.caption, color: .text(.light))
-            }
-          }
-        }
-      }
-
-      Section("Data") {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-          ForEach(Color.DataColor.allCases, id: \.self) { color in
-            VStack {
-              shape.foregroundColor(.data(color))
-              Text(color.rawValue).pbFont(.caption, color: .text(.light))
-            }
-          }
-        }
-      }
-
-      Section("Product Background") {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-          ForEach(Color.ProductColor.allCases, id: \.self) { color in
-            VStack {
-              shape.foregroundColor(.product(color, category: .background))
-              Text(color.rawValue.uppercased()).pbFont(.body(.smallest), color: .text(.light))
             }
           }
         }
@@ -96,17 +54,6 @@ struct ColorsCatalog: View {
           }
         }
       }
-
-      Section("Category") {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-          ForEach(Color.CategoryColor.allCases, id: \.self) { color in
-            VStack {
-              shape.foregroundColor(.category(color))
-              Text(color.rawValue.uppercased()).pbFont(.buttonText(10), color: .text(.light))
-            }
-          }
-        }
-      }
     }
   }
 }
@@ -116,4 +63,3 @@ struct ColorsCatalog_Previews: PreviewProvider {
     ColorsCatalog()
   }
 }
-#endif
