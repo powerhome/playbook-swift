e36af4f822a9b178b6e7cbd384caf80929750fc8
Isis Silva <isisms2602@gmail.com>
2023-05-23 17:30:17 -0300

    Fix colors catalog

--- Sources/Playbook/Design Elements/Colors/ColorsCatalog.swift
+++ Sources/Playbook/Design Elements/Colors/ColorsCatalog.swift
@@ -8,89 +8,89 @@
 import SwiftUI
 
 public struct ColorsCatalog: View {
-
-  public init() {}
-
   public var body: some View {
     let shape = Circle().frame(width: 60).pbShadow(.deep)
-    List {
-      Section("Text") {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-          ForEach(Color.TextColor.allCases, id: \.self) { color in
-            VStack {
-              shape.foregroundColor(Color.text(color))
-              Text(color.rawValue.capitalized).pbFont(.subcaption, color: .text(.light))
-            }
+    let grid = [GridItem(.adaptive(minimum: 80), spacing: 16)]
+    let textSection = Section("Text") {
+
+      LazyVGrid(columns: grid) {
+        ForEach(Color.TextColor.allCases, id: \.self) { color in
+          VStack {
+            shape.foregroundColor(Color.text(color))
+            Text(color.rawValue.uppercased()).pbFont(.caption, color: .text(.light)).frame(width: 60)
           }
         }
       }
-
-      Section("Background") {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-          ForEach(Color.BackgroundColor.allCases, id: \.self) { color in
-            VStack {
-              shape.foregroundColor(.background(color))
-              Text(color.rawValue.capitalized).pbFont(.subcaption, color: .text(.light))
-            }
-          }
-        }
-      }
-
-      Section("Status") {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-          ForEach(Color.StatusColor.allCases, id: \.self) { color in
-            VStack {
-              shape.foregroundColor(.status(color))
-              Text(color.rawValue).pbFont(.subcaption, color: .text(.light))
-            }
-          }
-        }
-      }
-
-      Section("Status Subtle") {
-        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-          ForEach(Color.StatusColor.allCases, id: \.self) { color in
-            VStack {
-              shape.foregroundColor(Color.status(color, subtle: true))
-              Text(color.rawValue).pbFont(.subcaption, color: .text(.light))
-            }
-          }
-        }
-      }
-//
-//      Section("Product Background") {
-//        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-//          ForEach(Color.ProductColor.allCases, id: \.self) { color in
-//            VStack {
-//              shape.foregroundColor(.product(color, category: .background))
-//              Text(color.rawValue.capitalized).pbFont(.subcaption, color: .text(.light))
-//            }
-//          }
-//        }
-//      }
-//
-//      Section("Product Highlight") {
-//        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-//          ForEach(Color.ProductColor.allCases, id: \.self) { color in
-//            VStack {
-//              shape.foregroundColor(.product(color, category: .highlight))
-//              Text(color.rawValue.capitalized).pbFont(.subcaption, color: .text(.light))
-//            }
-//          }
-//        }
-//      }
-
-      //      Section("Category") {
-      //        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-      //          ForEach(Color.CategoryColor.allCases, id: \.self) { color in
-      //            VStack {
-      //              shape.foregroundColor(.category(color))
-      //              Text(color.rawValue.capitalized).pbFont(.subcaption, color: .text(.light))
-      //            }
-      //          }
-      //        }
-      //      }
     }
-    .navigationTitle("Colors")
+
+    let backgroundSection = Section("Background") {
+      LazyVGrid(columns: grid) {
+        ForEach(Color.BackgroundColor.allCases, id: \.self) { color in
+          VStack {
+            shape.foregroundColor(.background(color))
+            Text(color.rawValue).pbFont(.caption, color: .text(.light))
+          }
+        }
+      }
+    }
+
+    let statusSection = Section("Status") {
+      LazyVGrid(columns: grid) {
+        ForEach(Color.StatusColor.allCases, id: \.self) { color in
+          VStack {
+            shape.foregroundColor(.status(color))
+            Text(color.rawValue).pbFont(.caption, color: .text(.light))
+          }
+        }
+      }
+    }
+
+    let productHighlightSection = Section("Product Highlight") {
+      LazyVGrid(columns: grid) {
+        ForEach(Color.ProductColor.allCases, id: \.self) { color in
+          VStack {
+            shape.foregroundColor(.product(color, category: .highlight))
+            Text(color.rawValue.uppercased()).pbFont(.body(.smallest), color: .text(.light))
+          }
+        }
+      }
+    }
+
+    let productBackgroundSection = Section("Product Background") {
+      LazyVGrid(columns: grid) {
+        ForEach(Color.ProductColor.allCases, id: \.self) { color in
+          VStack {
+            shape.foregroundColor(.product(color, category: .background))
+            Text(color.rawValue.uppercased()).pbFont(.body(.smallest), color: .text(.light))
+          }
+        }
+      }
+    }
+
+    let categorySection = Section("Category") {
+      LazyVGrid(columns: grid) {
+        ForEach(Color.CategoryColor.allCases, id: \.self) { color in
+          VStack {
+            shape.foregroundColor(.category(color))
+            Text(color.rawValue.uppercased()).pbFont(.body(.smallest), color: .text(.light))
+          }
+        }
+      }
+    }
+
+    return List {
+      textSection
+      backgroundSection
+      statusSection
+      productHighlightSection
+      productBackgroundSection
+      categorySection
+    }
+  }
+}
+
+struct ColorsCatalog_Previews: PreviewProvider {
+  static var previews: some View {
+    ColorsCatalog()
   }
 }
