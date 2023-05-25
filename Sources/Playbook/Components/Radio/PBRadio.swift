db08b4f2c1666f3e12d1d56a30d0c3efa6275f92
Israel.Molestina <molestinaisrael@gmail.com>
2023-05-25 15:39:34 -0400

    refactored radio kit: added four new props, alignment, spacing, padding and errorState

--- Sources/Playbook/Components/Radio/PBRadio.swift
+++ Sources/Playbook/Components/Radio/PBRadio.swift
@@ -23,36 +23,67 @@ public struct PBRadioItem: Identifiable, Equatable {
 public struct PBRadio: View {
   let items: [PBRadioItem]
   let orientation: Orientation
+  let alignment: Orientation
+  let spacing: CGFloat
+  let padding: CGFloat
+  let errorState: Bool
   @Binding var selectedItem: PBRadioItem?
 
-  public init(items: [PBRadioItem], orientation: Orientation = .vertical, selected: Binding<PBRadioItem?>) {
+  public init(
+    items: [PBRadioItem],
+    orientation: Orientation = .vertical,
+    alignment: Orientation = .horizontal,
+    spacing: CGFloat = Spacing.xSmall,
+    padding: CGFloat = Spacing.xSmall,
+    selected: Binding<PBRadioItem?>,
+    errorState: Bool = false
+  ) {
     self.items = items
     self.orientation = orientation
+    self.alignment = alignment
+    self.spacing = spacing
+    self.padding = padding
+    self.errorState = errorState
     _selectedItem = selected
   }
 
   public var body: some View {
     switch orientation {
     case .vertical:
-      VStack(alignment: .leading, spacing: Spacing.medium) {
+      VStack(alignment: .leading, spacing: spacing) {
         ForEach(items) { item in
           Button(item.title) {
             selectedItem = item
           }
-          .buttonStyle(PBRadioButtonStyle(subtitle: item.subtitle, isSelected: selectedItem?.id == item.id))
+          .buttonStyle(
+            PBRadioButtonStyle(
+              subtitle: item.subtitle,
+              isSelected: selectedItem?.id == item.id,
+              alignment: alignment,
+              padding: padding,
+              errorState: errorState
+            )
+          )
         }
       }
 
     case .horizontal:
-      HStack {
+      HStack(spacing: spacing) {
         ForEach(items) { item in
           Button(item.title) {
             selectedItem = item
           }
-          .buttonStyle(PBRadioButtonStyle(subtitle: item.subtitle, isSelected: selectedItem?.id == item.id))
+          .buttonStyle(
+            PBRadioButtonStyle(
+              subtitle: item.subtitle,
+              isSelected: selectedItem?.id == item.id,
+              alignment: alignment,
+              padding: padding,
+              errorState: errorState
+            )
+          )
         }
       }
-      .padding()
     }
   }
 }
@@ -62,38 +93,71 @@ public struct PBRadio: View {
 public struct PBRadioButtonStyle: ButtonStyle {
   private let subtitle: String
   private let isSelected: Bool
+  private let alignment: Orientation
+  private let padding: CGFloat
+  private let errorState: Bool
 
   private var borderColor: Color {
     isSelected ? .pbPrimary : .border
+  }
+
+  private var erroBorderColor: Color {
+    isSelected ? .pbPrimary : .red
   }
 
   private var lineWidth: CGFloat {
     isSelected ? 6 : 2
   }
 
-  public init(subtitle: String?, isSelected: Bool) {
+  public init(
+    subtitle: String?,
+    isSelected: Bool,
+    alignment: Orientation,
+    padding: CGFloat,
+    errorState: Bool
+  ) {
     self.isSelected = isSelected
     self.subtitle = subtitle ?? ""
+    self.alignment = alignment
+    self.padding = padding
+    self.errorState = errorState
   }
 
   public func makeBody(configuration: Configuration) -> some View {
-    HStack(alignment: .top) {
-      Circle()
-        .strokeBorder(borderColor, lineWidth: lineWidth)
-        .frame(width: 22, height: 22)
-      VStack(alignment: .leading, spacing: Spacing.xxSmall) {
-        configuration.label
-          .foregroundColor(.text(.default))
-          .pbFont(.body())
-          .frame(minHeight: 22)
-        if !subtitle.isEmpty {
-          Text(subtitle)
-            .foregroundColor(.text(.light))
-            .pbFont(.subcaption)
+    switch alignment {
+    case .horizontal:
+      HStack(alignment: .top, spacing: padding) {
+        Circle()
+          .strokeBorder(errorState == true ? erroBorderColor : borderColor, lineWidth: lineWidth)
+          .frame(width: 22, height: 22)
+        VStack(alignment: .leading, spacing: Spacing.xxSmall) {
+          configuration.label
+            .foregroundColor(errorState == true ? .status(.error) : .text(.default))
+            .pbFont(.body())
+            .frame(minHeight: 22)
+          if !subtitle.isEmpty {
+            Text(subtitle)
+              .foregroundColor(.text(.light))
+              .pbFont(.subcaption)
+          }
         }
       }
+      .contentShape(Rectangle())
+    case .vertical:
+      VStack {
+        Circle()
+          .strokeBorder(errorState == true ? erroBorderColor :  borderColor, lineWidth: lineWidth)
+          .frame(width: 22, height: 22)
+        VStack(alignment: .leading, spacing: Spacing.xxSmall) {
+          configuration.label
+            .foregroundColor(errorState == true ? .status(.error) :  .text(.default))
+            .pbFont(.body())
+            .frame(minHeight: 22)
+        }
+      }
+      .contentShape(Rectangle())
     }
-    .contentShape(Rectangle())
+
   }
 }
 
--- Sources/Playbook/Components/Radio/RadioCatalog.swift
+++ Sources/Playbook/Components/Radio/RadioCatalog.swift
@@ -1,6 +1,6 @@
 //
 //  RadioCatalog.swift
-//  
+//
 //
 //  Created by Isis Silva on 16/05/23.
 //
@@ -8,44 +8,157 @@
 import SwiftUI
 
 public struct RadioCatalog: View {
-  @State var selected: PBRadioItem? = PBRadioItem("Manor", subtitle: "subtitle")
+  @State var selected: PBRadioItem? = PBRadioItem("Power")
   var orientation: Orientation = .vertical
 
-  public init(selected: PBRadioItem? = nil, orientation: Orientation = .vertical) {
+  public init(
+    selected: PBRadioItem? = nil,
+    orientation: Orientation = .vertical
+  ) {
     self.selected = selected
     self.orientation = orientation
   }
 
   public var body: some View {
     List {
-      Section("Vertical") {
-        contentView(orientation: .vertical)
+      Section("Default") {
+        defaultView()
       }
-
-      Section("Horizontal") {
-        ScrollView(.horizontal) {
-          contentView(orientation: .horizontal)
-        }
+      Section("Custom") {
+        customView()
+      }
+      Section("With Error") {
+        errorView()
+      }
+      Section("Orientation") {
+        orientationView()
+      }
+      Section("Alignment") {
+        alignmentView()
+      }
+      Section("Spacing") {
+        spacingView()
+      }
+      Section("Padding") {
+        paddingView()
+      }
+      Section("Subtitle") {
+        subtitleView()
       }
     }
   }
 
-  func contentView(orientation: Orientation) -> some View {
+  func defaultView() -> some View {
     VStack(alignment: .leading) {
       PBRadio(
         items: [
-          PBRadioItem("Manor", subtitle: "subtitle"),
-          .init("Chalet"),
-          .init("Ranch"),
-          .init("Villa")
+          PBRadioItem("Power"),
+          .init("Nitro"),
+          .init("Google")
         ],
-        orientation: orientation,
+        orientation: .vertical,
         selected: $selected
       )
+    }
+  }
 
+  func	customView() -> some View {
+    VStack(alignment: .leading) {
       if let selected = selected {
-        Text("You selected: \(selected.title)")
+        Text("Your choice is: \(selected.title)")
       }
+      PBRadio(
+        items: [
+          PBRadioItem("Custom Power"),
+          .init("Custom Nitro"),
+          .init("Custom Google")
+        ],
+        orientation: .vertical,
+        selected: $selected
+      )
+    }
+  }
+
+  func	errorView() -> some View {
+    VStack(alignment: .leading) {
+      PBRadio(
+        items: [
+          PBRadioItem("Power")
+        ],
+        orientation: .vertical,
+        selected: $selected,
+        errorState: true
+      )
+    }
+  }
+
+  func	orientationView() -> some View {
+    VStack(alignment: .leading) {
+      PBRadio(
+        items: [
+          PBRadioItem("Power"),
+          .init("Nitro"),
+          .init("Google")
+        ],
+        orientation: .horizontal,
+        selected: $selected
+      )
+    }
+  }
+
+  func	alignmentView() -> some View {
+    VStack(alignment: .leading) {
+      PBRadio(
+        items: [
+          PBRadioItem("Power"),
+          .init("Nitro"),
+          .init("Google")
+        ],
+        orientation: .horizontal,
+        alignment: .vertical,
+        selected: $selected
+      )
+    }
+  }
+
+  func	spacingView() -> some View {
+    VStack(alignment: .leading) {
+      PBRadio(
+        items: [
+          PBRadioItem("Power"),
+          .init("Nitro"),
+          .init("Google")
+        ],
+        orientation: .vertical,
+        spacing: Spacing.medium,
+        selected: $selected
+      )
+    }
+  }
+
+  func	paddingView() -> some View {
+    VStack(alignment: .leading) {
+      PBRadio(
+        items: [
+          PBRadioItem("Power"),
+          .init("Nitro"),
+          .init("Google")
+        ],
+        orientation: .vertical,
+        padding: Spacing.medium,
+        selected: $selected
+      )
+    }
+  }
+
+  func	subtitleView() -> some View {
+    VStack(alignment: .leading) {
+      PBRadio(
+        items: [
+          PBRadioItem("Power", subtitle: "subtitle")
+        ],
+        selected: $selected
+      )
     }
   }
 }
