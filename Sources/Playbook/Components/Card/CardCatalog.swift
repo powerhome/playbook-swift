a38bacc33ee5a816e0caa426c5883a060d41b230
isis.silva <isisms2602@gmail.com>
2023-06-27 12:50:59 -0400

    Fix cards header preview

--- Sources/Playbook/Components/Card/CardCatalog.swift
+++ Sources/Playbook/Components/Card/CardCatalog.swift
@@ -69,12 +69,33 @@ public struct CardCatalog: View {
     .listRowSeparator(.hidden)
 
     let headers = Section("Header cards") {
-      ForEach(Color.DataColor.allCases, id: \.self) { color in
+      VStack {
         PBCard(padding: Spacing.none) {
-          PBCardHeader(color: .data(color)) {
-            Text(color.rawValue.capitalized).pbFont(.body(), color: .white).padding(Spacing.small)
+          PBCardHeader(color: .category(.category1)) {
+            Text("Category 1").pbFont(.body(), color: .white).padding(Spacing.small)
           }
-          Text(text).pbFont(.body(), color: .text(.default)).padding(Spacing.small)
+          Text("Body").pbFont(.body(), color: .text(.default)).padding(Spacing.small)
+        }
+
+        PBCard(padding: Spacing.none) {
+          PBCardHeader(color: .category(.category3)) {
+            Text("Category 3").pbFont(.body(), color: .black).padding(Spacing.small)
+          }
+          Text("Body").pbFont(.body(), color: .text(.default)).padding(Spacing.small)
+        }
+
+        PBCard(padding: Spacing.none) {
+          PBCardHeader(color: .product(.product2, category: .background)) {
+            Text("Product 2 Background").pbFont(.body(), color: .white).padding(Spacing.small)
+          }
+          Text("Body").pbFont(.body(), color: .text(.default)).padding(Spacing.small)
+        }
+
+        PBCard(padding: Spacing.none) {
+          PBCardHeader(color: .product(.product6, category: .background)) {
+            Text("Product 6 Background").pbFont(.body(), color: .white).padding(Spacing.small)
+          }
+          Text("Body").pbFont(.body(), color: .text(.default)).padding(Spacing.small)
         }
       }
     }
