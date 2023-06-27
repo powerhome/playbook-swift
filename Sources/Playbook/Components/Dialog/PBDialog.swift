bcde02f1e7f1150a33ba6342ccba102f35654c0e
Everton Cunha <ever.cunha@gmail.com>
2023-06-26 22:15:19 -0300

    [ROGUE-806] Fix release notes (#104)
    ## Summary
    - [First bullet point must be a **_short_** overall summary]
    
    ## Additional Details
    - [Runway Story]
    - [Optional: add more details]
    
    ## Screenshots (for UI stories: show before/after changes)
    
    | Before | After |
    | --------------------------------- | ---------------------------------
    |
    | [Before Pic] | [After Pic] |
    
    ## Breaking Changes
    
    [Yes/No (Explain)]
    
    ## Testing
    
    [Insert testing details or N/A]
    
    ## Checklist
    
    - [ ] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [ ] **SCREENSHOTS** - Please add a screenshot or two. For UI changes,
    you MUST provide before and after screenshots.
    - [ ] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [ ] **TESTING** - Have you tested your story?
    
    ---------
    
    Co-authored-by: Ísis <isis.silva@powerhrg.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Israel.Molestina <molestinaisrael@gmail.com>
    Co-authored-by: isis.silva <isisms2602@gmail.com>

--- Sources/Playbook/Components/Dialog/PBDialog.swift
+++ Sources/Playbook/Components/Dialog/PBDialog.swift
@@ -76,6 +76,7 @@ public struct PBDialog<Content: View>: View {
 
       case .status(let status):
         PBStatusDialogView(status: status, title: title ?? "", description: message ?? "")
+          .frame(maxWidth: .infinity)
       }
 
       if let confirmButton = confirmButton {
@@ -94,6 +95,7 @@ public struct PBDialog<Content: View>: View {
     .frame(maxWidth: variant.width(size))
     #endif
     .padding(padding)
+    .preferredColorScheme(.light)
   }
 
   func cancelButtonAction() -> (String, (() -> Void))? {
