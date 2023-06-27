7cb6a80441e561290a543af8720e8f09e1b3815a
Israel Molestina <molestinaisrael@gmail.com>
2023-06-26 21:35:40 -0400

    [PBIOS-44] 🛠️ Validation: Radio Component (#88)
    ## Summary
    * Refactored the Radio kit and added four new props: alignment, spacing,
    padding and errorState
    
    ## Additional Details
    - [Runway
    Story](https://nitro.powerhrg.com/runway/backlog_items/PBIOS-44)
    * Alignment: Orientation = Horizontal or vertical order of the Circle
    button and the text
    * Spacing: CGFloat = spacing between each radio button when there are
    more than one radio buttons passed into the Radio kit
    * Padding: CGFloat = Spacing between the Circle button and the Text
    * errorState: Bool = boolean that gives the radio button error styling
    
    ## Screenshots (for UI stories: show before/after changes)
    
    | Before | After |
    | --------------------------------- | ---------------------------------
    |
    | ![Screenshot 2023-05-25 at 3 50 39
    PM](https://github.com/powerhome/PlaybookSwift/assets/73671109/368f48fd-ef64-4e5f-b869-7e8e83ffe891)
    | ![Screenshot 2023-05-25 at 3 41 00
    PM](https://github.com/powerhome/PlaybookSwift/assets/73671109/ae43ce09-fbfe-431c-afca-3b7e6471665d)
    ![Screenshot 2023-05-25 at 3 41 07
    PM](https://github.com/powerhome/PlaybookSwift/assets/73671109/6683e2f0-3a26-42ab-a5d1-5edebf617193)|
    
    
    
    ## Breaking Changes
    
    NO
    
    ## Testing
    
    Tested locally in Previews.
    
    ## Checklist
    
    - [x] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [x] **SCREENSHOTS** - Please add a screenshot or two. For UI changes,
    you MUST provide before and after screenshots.
    - [x] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [x] **TESTING** - Have you tested your story?

---
+++ Sources/Playbook/Components/Time And Date/TimeStampCatalog.swift
@@ -0,0 +1,105 @@
+//
+//  TimeStampCatalog.swift
+//
+//
+//  Created by Israel Molestina on 5/23/23.
+//
+
+import SwiftUI
+
+public struct TimeStampCatalog: View {
+
+  public init() {}
+
+  public var body: some View {
+    let timeInterval: TimeInterval = 31_536_000
+    let minWidth: CGFloat = 0
+    List {
+      Section("Default") {
+        VStack(alignment: .leading, spacing: nil) {
+          PBTimestamp(Date(), showDate: false)
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date())
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date().addingTimeInterval(timeInterval))
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+        }
+      }
+
+      Section("Alignments") {
+        VStack(alignment: .leading, spacing: nil) {
+          PBTimestamp(Date(), showDate: false)
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date())
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date().addingTimeInterval(timeInterval))
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+
+          PBTimestamp(Date(), showDate: false)
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .center)
+          PBTimestamp(Date())
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .center)
+          PBTimestamp(Date().addingTimeInterval(timeInterval))
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .center)
+
+          PBTimestamp(Date(), showDate: false)
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .trailing)
+          PBTimestamp(Date())
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .trailing)
+          PBTimestamp(Date().addingTimeInterval(timeInterval))
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .trailing)
+        }
+      }
+
+      Section("Timezones") {
+        VStack(alignment: .leading, spacing: nil) {
+          PBTimestamp(Date(), showDate: false, showTimeZone: true, timeZone: "America/New_York")
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date(), showTimeZone: true, timeZone: "America/New_York")
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(
+            Date().addingTimeInterval(timeInterval),
+            showTimeZone: true,
+            timeZone: "America/New_York"
+          )
+          .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+
+          PBTimestamp(Date(), showDate: false, showTimeZone: true, timeZone: "Asia/Hong_Kong")
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date(), showTimeZone: true, timeZone: "Asia/Hong_Kong")
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date().addingTimeInterval(timeInterval), showTimeZone: true, timeZone: "Asia/Hong_Kong")
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+        }
+      }
+
+      Section("Last Updated By") {
+        VStack(alignment: .leading, spacing: nil) {
+          PBTimestamp(
+            Date().addingTimeInterval(-12),
+            showUser: true,
+            text: "Andrew Black",
+            variant: .updated
+          )
+          .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date().addingTimeInterval(-12), variant: .updated)
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+        }
+      }
+
+      Section("Time Ago") {
+        VStack(alignment: .leading, spacing: nil) {
+          PBTimestamp(
+            Date().addingTimeInterval(-10),
+            showUser: true,
+            text: "Andrew Black",
+            variant: .elapsed
+          )
+          .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+          PBTimestamp(Date().addingTimeInterval(-88000), variant: .elapsed)
+            .frame(minWidth: minWidth, maxWidth: .infinity, alignment: .leading)
+        }
+      }
+    }
+  }
+}
