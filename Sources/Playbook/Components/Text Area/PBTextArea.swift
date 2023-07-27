675e90222315251fed8ed308fa7c5ee89aaa9c51
isis.silva <isisms2602@gmail.com>
2023-07-27 15:55:09 -0400

    fix inline text area variant

--- Sources/Playbook/Components/Text Area/PBTextArea.swift
+++ Sources/Playbook/Components/Text Area/PBTextArea.swift
@@ -7,8 +7,72 @@
 
 import SwiftUI
 
-public extension PBTextArea {
+public struct PBTextArea: View {
+  var characterCount: CharacterCount
+  var inline: Bool
+  var label: String?
+  var placeholder: String?
 
+  @FocusState private var isTextAreaFocused: Bool
+  @Binding var text: String
+  @State var error: String?
+
+  public init(
+    _ label: String? = nil,
+    text: Binding<String>,
+    placeholder: String? = nil,
+    error: String? = nil,
+    inline: Bool = false,
+    characterCount: CharacterCount = .noCount
+  ) {
+    self.label = label
+    _text = text
+    self.placeholder = placeholder
+    self.error = error
+    self.inline = inline
+    self.characterCount = characterCount
+  }
+
+  public var body: some View {
+    VStack(alignment: .leading, spacing: Spacing.xxSmall) {
+      if let label = label {
+        Text(label)
+          .pbFont(.caption)
+          .padding(.bottom, Spacing.xSmall)
+      }
+      if inline {
+        PBCard(border: isTextAreaFocused, padding: 0, style: style(isTextAreaFocused)) {
+          inlineTextEditorView
+          if let placeholder = placeholder, (isTextAreaFocused == false && text.isEmpty) {
+            placeHolderTextView(placeholder, topPadding: -40)
+          }
+        }
+      } else {
+        PBCard(padding: 0, style: style(isTextAreaFocused)) {
+          ZStack(alignment: .leading) {
+            textEditorView
+            if let placeholder = placeholder, (isTextAreaFocused == false && text.isEmpty) {
+              placeHolderTextView(placeholder, topPadding: -32)
+            }
+          }
+        }
+      }
+      countView
+    }
+    .onAppear {
+      if case let .maxCharacterCountError(maxCount, message) = characterCount {
+        error = text.count >= maxCount ? message : nil
+      }
+    }
+    .onChange(of: text) { text in
+      if case let .maxCharacterCountError(maxCount, message) = characterCount {
+        error = text.count >= maxCount ? message : nil
+      }
+    }
+  }
+}
+
+public extension PBTextArea {
   func placeHolderTextView(_ placeholder: String, topPadding: CGFloat) -> some View {
     Text(placeholder)
       .padding(.top, topPadding)
@@ -44,15 +108,16 @@ public extension PBTextArea {
 
   var inlineTextEditorView: some View {
     TextEditor(text: editorText)
-      .padding(.top, 10)
-      .padding(.horizontal, 12)
       .foregroundColor(.text(.default))
       .pbFont(.body())
+      .padding(.horizontal, 12)
+      .frame(minWidth: 44)
       .focused($isTextAreaFocused, equals: true)
       .accentColor(.text(.default))
       .onTapGesture {
         isTextAreaFocused = true
       }
+      .frame(height: 44)
   }
 
   func style(_ isTextAreaFocused: Bool) -> PBCardStyle {
@@ -103,76 +168,11 @@ public extension PBTextArea {
   enum CharacterCount: Equatable {
     case noCount, count, maxCharacterCount(Int), maxCharacterCountBlock(Int), maxCharacterCountError(Int, String)
   }
-
-}
-
-public struct PBTextArea: View {
-  var characterCount: CharacterCount
-  var inline: Bool
-  var label: String
-  var placeholder: String?
-
-  @FocusState private var isTextAreaFocused: Bool
-  @Binding var text: String
-  @State var error: String?
-
-  public init(
-    _ label: String,
-    text: Binding<String>,
-    placeholder: String? = nil,
-    error: String? = nil,
-    inline: Bool = false,
-    characterCount: CharacterCount = .noCount
-  ) {
-    self.label = label
-    _text = text
-    self.placeholder = placeholder
-    self.error = error
-    self.inline = inline
-    self.characterCount = characterCount
-  }
-  public var body: some View {
-    VStack(alignment: .leading, spacing: Spacing.xxSmall) {
-      Text(label)
-        .pbFont(.caption)
-        .padding(.bottom, Spacing.xSmall)
-      if inline {
-        PBCard(border: isTextAreaFocused, padding: 0, style: style(isTextAreaFocused)) {
-          inlineTextEditorView
-          if let placeholder = placeholder, (isTextAreaFocused == false && text.isEmpty) {
-            placeHolderTextView(placeholder, topPadding: -40)
-          }
-        }
-      } else {
-        PBCard(padding: 0, style: style(isTextAreaFocused)) {
-          ZStack(alignment: .leading) {
-            textEditorView
-            if let placeholder = placeholder, (isTextAreaFocused == false && text.isEmpty) {
-              placeHolderTextView(placeholder, topPadding: -32)
-            }
-          }
-        }
-      }
-      countView
-    }
-    .onAppear {
-      if case let .maxCharacterCountError(maxCount, message) = characterCount {
-        error = text.count >= maxCount ? message : nil
-      }
-    }
-    .onChange(of: text) { text in
-      if case let .maxCharacterCountError(maxCount, message) = characterCount {
-        error = text.count >= maxCount ? message : nil
-      }
-    }
-  }
 }
 
 struct PBTextArea_Previews: PreviewProvider {
   static var previews: some View {
     registerFonts()
-
     return TextAreaCatalog()
-      .background(Color.card)
   }
 }
