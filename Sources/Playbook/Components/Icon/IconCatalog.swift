a7d1135c199a53a3463d36503a17b6daa5f65488
Carlos Lima <carloslimasd@gmail.com>
2023-08-23 16:31:08 -0300

    Updating the Icon Showcase doc examples

--- Sources/Playbook/Components/Icon/IconCatalog.swift
+++ Sources/Playbook/Components/Icon/IconCatalog.swift
@@ -9,43 +9,46 @@ import SwiftUI
 
 public struct IconCatalog: View {
   public var body: some View {
-    List {
-      Section("Default") {
-        PBIcon.fontAwesome(.user, size: .x1)
-      }
-
-      Section("Rotate") {
-        HStack {
-          PBIcon(FontAwesome.user, rotation: .right)
-          PBIcon(FontAwesome.user, rotation: .zero)
-          PBIcon(FontAwesome.user, rotation: .obtuse)
+    ScrollView {
+      VStack(spacing: Spacing.medium) {
+        PBDoc(title: "Default") {
+          PBIcon.fontAwesome(.user, size: .x1)
         }
-      }
 
-      Section("Flipped") {
-        HStack {
-          PBIcon(FontAwesome.questionCircle, flipped: [.horizontal])
-          PBIcon(FontAwesome.questionCircle, flipped: [.vertical])
-          PBIcon(FontAwesome.questionCircle, flipped: [.horizontal, .vertical])
-        }
-      }
-
-      Section("Border") {
-        HStack {
-          PBIcon(FontAwesome.user, border: true)
-        }
-      }
-
-      Section("Size") {
-        ForEach(PBIcon.IconSize.allCases, id: \.fontSize) { size in
-          HStack {
-            PBIcon.fontAwesome(.atlas, size: size)
-            Text(size.rawValue)
+        PBDoc(title: "Rotate") {
+          HStack(spacing: Spacing.xSmall) {
+            PBIcon(FontAwesome.user, rotation: .right)
+            PBIcon(FontAwesome.user, rotation: .zero)
+            PBIcon(FontAwesome.user, rotation: .obtuse)
           }
         }
-        .listRowSeparator(.hidden)
+
+        PBDoc(title: "Flipped") {
+          HStack(spacing: Spacing.xSmall) {
+            PBIcon(FontAwesome.questionCircle, flipped: [.horizontal])
+            PBIcon(FontAwesome.questionCircle, flipped: [.vertical])
+            PBIcon(FontAwesome.questionCircle, flipped: [.horizontal, .vertical])
+          }
+        }
+
+        PBDoc(title: "Border") {
+          PBIcon(FontAwesome.user, border: true)
+        }
+
+        PBDoc(title: "Size") {
+          VStack(alignment: .leading, spacing: Spacing.small) {
+            ForEach(PBIcon.IconSize.allCases, id: \.fontSize) { size in
+              HStack(spacing: Spacing.xSmall) {
+                PBIcon.fontAwesome(.atlas, size: size)
+                Text(size.rawValue)
+              }
+            }
+          }
+        }
       }
+      .padding(Spacing.medium)
     }
+    .background(Color.background(Color.BackgroundColor.light))
     .navigationTitle("Icon")
   }
 }
