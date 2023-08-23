beda35ffd77142d2482a36bf96eda81054bad8c7
isis.silva <isisms2602@gmail.com>
2023-08-23 01:32:51 -0400

    add catalog file

---
+++ Sources/Playbook/Components/Typeahead/TypeaheadCatalog.swift
@@ -0,0 +1,20 @@
+//
+//  TypeaheadCatalog.swift
+//  
+//
+//  Created by Isis Silva on 23/08/23.
+//
+
+import SwiftUI
+
+struct TypeaheadCatalog: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TypeaheadCatalog_Previews: PreviewProvider {
+    static var previews: some View {
+        TypeaheadCatalog()
+    }
+}
