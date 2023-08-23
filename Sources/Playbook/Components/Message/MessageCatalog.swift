a964b65573280efb5ca7a9e8b31b05cfb10a6260
Carlos Lima <carloslimasd@gmail.com>
2023-08-23 17:14:36 -0300

    Updating the Message Showcase doc examples

--- Sources/Playbook/Components/Message/MessageCatalog.swift
+++ Sources/Playbook/Components/Message/MessageCatalog.swift
@@ -9,91 +9,87 @@ import SwiftUI
 
 public struct MessageCatalog: View {
   public var body: some View {
-    List {
-      Section("Default") {
-        PBMessage(
-          avatar: AnyView(picAnna),
-          label: "Anna Black",
-          message: "How can we assist you today?",
-          timestamp: Date().addingTimeInterval(-20)
-        )
-        .padding(.vertical)
+    ScrollView {
+      VStack(spacing: Spacing.medium) {
+        PBDoc(title: "Default") {
+          VStack(alignment: .leading, spacing: Spacing.small) {
+            PBMessage(
+              avatar: AnyView(picAnna),
+              label: "Anna Black",
+              message: "How can we assist you today?",
+              timestamp: Date().addingTimeInterval(-20)
+            )
 
-        PBMessage(
-          avatar: AnyView(picPatric),
-          label: "Patrick Welch",
-          message: "We will escalate this issue to a Senior Support agent.",
-          timestamp: Date().addingTimeInterval(-540),
-          timestampAlignment: .leading
-        )
-        .padding(.vertical)
+            PBMessage(
+              avatar: AnyView(picPatric),
+              label: "Patrick Welch",
+              message: "We will escalate this issue to a Senior Support agent.",
+              timestamp: Date().addingTimeInterval(-540),
+              timestampAlignment: .leading
+            )
 
-        PBMessage(
-          avatar: AnyView(picLuccile),
-          label: "Lucille Sanchez",
-          message: "Application for Kate Smith is waiting for your approval",
-          timestamp: Date().addingTimeInterval(-200000)
-        )
-        .padding(.vertical)
+            PBMessage(
+              avatar: AnyView(picLuccile),
+              label: "Lucille Sanchez",
+              message: "Application for Kate Smith is waiting for your approval",
+              timestamp: Date().addingTimeInterval(-200000)
+            )
 
-        PBMessage(
-          avatar: AnyView(PBAvatar(name: "Beverly Reyes", size: .xSmall)),
-          label: "Beverly Reyes",
-          message: "We are so sorry you had a bad experience!",
-          timestamp: Date().addingTimeInterval(-200000)
-        )
-        .padding(.vertical)
+            PBMessage(
+              avatar: AnyView(PBAvatar(name: "Beverly Reyes", size: .xSmall)),
+              label: "Beverly Reyes",
+              message: "We are so sorry you had a bad experience!",
+              timestamp: Date().addingTimeInterval(-200000)
+            )
 
-        PBMessage(
-          label: "Keith Craig",
-          message: "Please hold for one moment, I will check with my manager.",
-          timestamp: Date().addingTimeInterval(-200000)
-        ) {}
-          .padding(.vertical)
+            PBMessage(
+              label: "Keith Craig",
+              message: "Please hold for one moment, I will check with my manager.",
+              timestamp: Date().addingTimeInterval(-200000)
+            ) {}
 
-        PBMessage(
-          label: "Keith Craig",
-          timestamp: Date().addingTimeInterval(-200000)
-        ) {
-          Image("Forest", bundle: .module).resizable().frame(width: 240, height: 240)
+            PBMessage(
+              label: "Keith Craig",
+              timestamp: Date().addingTimeInterval(-200000)
+            ) {
+              Image("Forest", bundle: .module).resizable().frame(width: 240, height: 240)
+            }
+
+            PBMessage(
+              label: "Keith Craig",
+              message: "Please hold for one moment, I will check with my manager.",
+              timestamp: Date().addingTimeInterval(-200000)
+            ) {
+              Image("Forest", bundle: .module).resizable().frame(width: 240, height: 240)
+            }
+          }
         }
-        .padding(.vertical)
 
-        PBMessage(
-          label: "Keith Craig",
-          message: "Please hold for one moment, I will check with my manager.",
-          timestamp: Date().addingTimeInterval(-200000)
-        ) {
-          Image("Forest", bundle: .module).resizable().frame(width: 240, height: 240)
-        }
-        .padding(.vertical)
+        #if os(macOS)
+          PBDoc(title: "With timestamp hover") {
+            PBMessage(
+              avatar: AnyView(avatarXSmall),
+              label: userName,
+              message: message,
+              timestamp: Date(),
+              timestampAlignment: .leading,
+              changeTimeStampOnHover: true
+            )
+
+            PBMessage(
+              avatar: AnyView(avatarXSmall),
+              label: userName,
+              message: message,
+              timestamp: Date(),
+              timestampAlignment: .trailing,
+              changeTimeStampOnHover: true
+            )
+          }
+        #endif
       }
-      .listRowSeparator(.hidden)
-
-      #if os(macOS)
-        Section("With timestamp hover") {
-          PBMessage(
-            avatar: AnyView(avatarXSmall),
-            label: userName,
-            message: message,
-            timestamp: Date(),
-            timestampAlignment: .leading,
-            changeTimeStampOnHover: true
-          )
-          .padding(.vertical)
-
-          PBMessage(
-            avatar: AnyView(avatarXSmall),
-            label: userName,
-            message: message,
-            timestamp: Date(),
-            timestampAlignment: .trailing,
-            changeTimeStampOnHover: true
-          )
-          .padding(.vertical)
-        }
-      #endif
+      .padding(Spacing.medium)
     }
+    .background(Color.background(Color.BackgroundColor.light))
     .navigationTitle("Message")
   }
 }
