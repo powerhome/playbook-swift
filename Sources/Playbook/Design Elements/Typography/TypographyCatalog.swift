866963e2bf5b8a2fb1951141d31f431569366eda
Carlos Lima <carloslimasd@gmail.com>
2023-09-19 13:23:55 -0300

    Validating Title letter spacing and font weight

--- Sources/Playbook/Design Elements/Typography/TypographyCatalog.swift
+++ Sources/Playbook/Design Elements/Typography/TypographyCatalog.swift
@@ -9,20 +9,33 @@ import SwiftUI
 
 public struct TypographyCatalog: View {
   public var body: some View {
-    let title = Section("Title") {
-      Text("Title 1\nTitle 1")
-        .pbFont(.title1)
-      Text("Title 2\nTitle 2")
-        .pbFont(.title2)
-      Text("Title 3\nTitle 3")
-        .pbFont(.title3)
-      Text("Title 4\nTitle 4")
-        .pbFont(.title4)
-      Text("Title 4 Link Variant")
-        .pbFont(.title4, variant: .link)
+    let title = PBDoc(title: "Title") {
+      VStack(alignment: .leading, spacing: Spacing.small) {
+        Text("Title 1")
+          .pbFont(.title1)
+        Text("Title 2")
+          .pbFont(.title2)
+        Text("Title 3")
+          .pbFont(.title3)
+        Text("Title 4")
+          .pbFont(.title4)
+        Text("Title 4 Link Variant")
+          .pbFont(.title4, variant: .link)
+      }
     }
 
-    let body = Section("Text size") {
+    let titleLight = PBDoc(title: "Title Light Weight") {
+      VStack(alignment: .leading, spacing: Spacing.small) {
+        Text("Title 1")
+          .pbFont(.title1, variant: .light)
+        Text("Title 2")
+          .pbFont(.title2, variant: .light)
+        Text("Title 3")
+          .pbFont(.title3, variant: .light)
+      }
+    }
+
+    let body = PBDoc(title: "Text size") {
       ForEach(TextSize.Body.allCases, id: \.rawValue) { size in
         Text("Text size \(Int(size.rawValue)) px")
           .pbFont(.monogram(size.rawValue))
@@ -30,20 +43,20 @@ public struct TypographyCatalog: View {
       }
     }
 
-    let letterSpacing = Section("Letter spacing") {
+    let letterSpacing = PBDoc(title: "Letter spacing") {
       ForEach(LetterSpacing.allCases, id: \.rawValue) { space in
         Text(space.rawValue).tracking(PBFont.body.space(space, font: .body))
       }
     }
 
-    let componentsText = Section("Components Text") {
+    let componentsText = PBDoc(title: "Components Text") {
       Text("Button Text")
         .pbFont(.buttonText())
       Text("Badge Text")
         .pbFont(.badgeText)
     }
 
-    let caption = Section("Caption") {
+    let caption = PBDoc(title: "Caption") {
       Text("Large Caption")
         .pbFont(.largeCaption)
       Text("Caption")
@@ -54,25 +67,28 @@ public struct TypographyCatalog: View {
         .pbFont(.subcaption, variant: .link)
     }
 
-    let detail = Section("Detail") {
+    let detail = PBDoc(title: "Detail") {
       Text("I am a detail kit")
         .pbFont(.detail(false))
-
       Text("I am a detail kit")
         .pbFont(.detail(true))
-
     }
 
-    return List {
-      title
-      body
-      if #available(iOS 16.0, *) {
-        letterSpacing
+    return ScrollView {
+      VStack(spacing: Spacing.medium) {
+        title
+        titleLight
+        body
+        if #available(iOS 16.0, *) {
+          letterSpacing
+        }
+        componentsText
+        caption
+        detail
       }
-      componentsText
-      caption
-      detail
+      .padding(Spacing.medium)
     }
+    .background(Color.background(Color.BackgroundColor.light))
     .navigationTitle("Typography")
   }
 }
