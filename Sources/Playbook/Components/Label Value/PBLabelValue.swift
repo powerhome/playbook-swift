32bdfbaeef2de401e7e228c382caad6a0a01a0b0
Carlos Lima <carloslimasd@gmail.com>
2023-10-02 12:54:45 -0300

    [PBIOS-134] Validating Label Value kit (#156)
    ## Summary
    - Validating Label Value kit
    
    ## Additional Details
    - [Runway
    Story](https://nitro.powerhrg.com/runway/backlog_items/PBIOS-134)
    
    ## Screenshots (for UI stories: show before/after changes)
    ![Screenshot 2023-09-25 at 11 55
    43](https://github.com/powerhome/PlaybookSwift/assets/2573205/b3b2fb70-2544-463e-a35b-2bbaeaf8c001)
    
    ## Breaking Changes
    No
    
    ## Checklist
    
    - [x] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [x] **SCREENSHOTS** - Please add a screenshot or two. For UI changes,
    you MUST provide before and after screenshots.
    - [ ] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [x] **TESTING** - Have you tested your story?

--- Sources/Playbook/Components/Label Value/PBLabelValue.swift
+++ Sources/Playbook/Components/Label Value/PBLabelValue.swift
@@ -9,22 +9,87 @@ import SwiftUI
 
 public struct PBLabelValue: View {
   private let label: String
-  private let value: String
+  private let value: String?
+  private let variant: Variant?
+  private let icon: PlaybookGenericIcon?
+  private let description: String?
+  private let title: String?
+  private let date: Date?
+  private let active: Bool?
 
-  public init(_ label: String, _ value: String) {
+  public init(
+    _ label: String,
+    _ value: String? = nil,
+    variant: Variant? = .default,
+    icon: PlaybookGenericIcon? = nil,
+    description: String? = nil,
+    title: String? = nil,
+    date: Date? = nil,
+    active: Bool? = false
+  ) {
     self.label = label
     self.value = value
+    self.variant = variant
+    self.icon = icon
+    self.description = description
+    self.title = title
+    self.date = date
+    self.active = active
   }
 
   public var body: some View {
+    let textVariant: Typography.Variant = active == true ? .link : .none
+
     VStack(alignment: .leading, spacing: Spacing.xxSmall) {
       Text(label)
         .padding(.bottom, 5.3)
         .pbFont(.caption)
-      Text(value)
-        .foregroundColor(.text(.default))
-        .pbFont(.body)
+
+      if variant == .details {
+        HStack(spacing: Spacing.xSmall) {
+          if let icon = icon {
+            PBIcon(icon, size: .small)
+              .foregroundColor(.text(.light))
+          }
+
+          if let description = description {
+            Text(description)
+              .foregroundColor(.text(.light))
+              .pbFont(.body)
+          }
+
+          if let title = title {
+            Text(title)
+              .pbFont(.title4, variant: textVariant)
+          }
+
+          if let date = date {
+            Text(formatDate(date))
+              .pbFont(.title4, variant: textVariant)
+          }
+        }
+      } else {
+        if let value = value {
+          Text(value)
+            .foregroundColor(.text(.default))
+            .pbFont(.body)
+        }
+      }
     }
+  }
+}
+
+public extension PBLabelValue {
+  enum Variant {
+    case `default`
+    case details
+  }
+
+  func formatDate(_ date: Date) -> String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "MM/dd"
+
+    return "· " + formatter.string(from: date)
   }
 }
 
