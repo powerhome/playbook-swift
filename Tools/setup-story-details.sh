c07306f5cea85318343ede35fc685b40051ff940
Stephen Marshall <smarshall1980@gmail.com>
2023-10-18 12:43:26 -0400

    🛠️ Prevent Renovate builds from commenting in Runway

--- Tools/setup-story-details.sh
+++ Tools/setup-story-details.sh
@@ -2,18 +2,18 @@
 
 export ROOT_DIR="$PWD"
 
-. "$ROOT_DIR/Tools/include.sh"
+. "$ROOT_DIR/Tools/github-utils.sh"
+
 export BUILD_ROOT="$ROOT_DIR/Build"
 export GITHUB_PULL_REQUEST_ID=$CHANGE_ID
+
 if [ -z "$GITHUB_COMMIT_SHA" ]; then
   export GITHUB_COMMIT_SHA=`git rev-parse HEAD`
 fi
 
 mkdir -p "$BUILD_ROOT"
 
-#
 # Nitro Runway
-#
 
 echo ""
 echo "****************************"
@@ -21,11 +21,15 @@ echo "*    Runway PR Details     *"
 echo "****************************"
 
 if [ -n "$GITHUB_PULL_REQUEST_ID" ]; then
-  export NITRO_PR_DETAILS="$BUILD_ROOT/pr-$GITHUB_PULL_REQUEST_ID-details.json"
+  export PR_DETAILS="$BUILD_ROOT/pr-$GITHUB_PULL_REQUEST_ID-details.json"
+
+  echo "details file is: $PR_DETAILS"
 
   getPullRequestDetails "$GITHUB_PULL_REQUEST_ID" \
-    | tee "$NITRO_PR_DETAILS" \
-    | jq ". | {url, id, number, title, state, commits, mergeable}"
+    | tee "$PR_DETAILS" \
+    | jq ". | {url, user, id, number, title, state, commits, mergeable}"
+
+  export PR_USER_HANDLE=`jq -r .user.login "$PR_DETAILS"`
 else
   echo "GITHUB_PULL_REQUEST_ID not set, skipping PR details"
 fi
@@ -35,15 +39,15 @@ echo "*********************************"
 echo "*    Runway Commit Statuses     *"
 echo "*********************************"
 
-if [ -n "$NITRO_PR_DETAILS" ]; then
-  export GITHUB_COMMIT_SHA=`jq -r .head.sha "$NITRO_PR_DETAILS"`
+if [ -n "$PR_DETAILS" ]; then
+  export GITHUB_COMMIT_SHA=`jq -r .head.sha "$PR_DETAILS"`
   export NITRO_COMMIT_STATUSES="$BUILD_ROOT/commit-$GITHUB_COMMIT_SHA-statuses.json"
 
   getCommitStatuses "$GITHUB_COMMIT_SHA" \
     | tee "$NITRO_COMMIT_STATUSES" \
     | jq "[.[] | {context, state, created_at}]"
 else
-  echo "NITRO_PR_DETAILS not set, skipping commit statuses"
+  echo "PR_DETAILS not set, skipping commit statuses"
 fi
 
 echo ""
@@ -56,7 +60,7 @@ BACKLOG_ITEM_ID_REGEX='s/.\([a-zA-Z0-9]\{1,\}-[0-9]\{1,\}\).*/\1/p'
 EXTRACT_BACKLOG_ITEM_CMD="sed -n $BACKLOG_ITEM_ID_REGEX"
 
 if [ -n "$GITHUB_PULL_REQUEST_ID" ]; then
-  export RUNWAY_BACKLOG_ITEM_ID=`jq -r .title "$NITRO_PR_DETAILS" | $EXTRACT_BACKLOG_ITEM_CMD`
+  export RUNWAY_BACKLOG_ITEM_ID=`jq -r .title "$PR_DETAILS" | $EXTRACT_BACKLOG_ITEM_CMD`
 
   if [ -n "$RUNWAY_BACKLOG_ITEM_ID" -a "$RUNWAY_BACKLOG_ITEM_ID" != "null" ]; then
     echo "RUNWAY_BACKLOG_ITEM_ID = $RUNWAY_BACKLOG_ITEM_ID"
