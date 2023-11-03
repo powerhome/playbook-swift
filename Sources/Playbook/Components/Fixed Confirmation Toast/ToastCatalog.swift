79ed2dc3a5d5419f732067a0588cfc0764d90119
RachelRadford21 <rachelaradford21@gmail.com>
2023-11-03 10:48:05 -0400

    Fixed Confirmation Toast

--- Sources/Playbook/Components/Fixed Confirmation Toast/ToastCatalog.swift
+++ Sources/Playbook/Components/Fixed Confirmation Toast/ToastCatalog.swift
@@ -1,15 +1,9 @@
-//
-//  SwiftUIView.swift
-//
-//
-//  Created by Isis Silva on 18/10/23.
-//
-
 import SwiftUI
 
+@available(iOS 16.0, *)
 public struct ToastCatalog: View {
   @State private var toastView: PBToast?
-  @State private var toastPosition: PBToast.Position = .top
+  @State private var position: PBToast.Position = .top
 
   private let message = "Design & Handoff Process was moved to UX Designer."
 
@@ -18,7 +12,6 @@ public struct ToastCatalog: View {
       VStack(spacing: Spacing.medium) {
         PBDoc(title: "Default") { defaultToast }
         PBDoc(title: "Multi Line") { multiLine }
-        PBDoc(title: "Click to Close") { clickToClose }
         PBDoc(title: "Simple") { positionButton }
         PBDoc(title: "Children") { children }
         PBDoc(title: "Dismiss with timer") { withTimer }
@@ -26,48 +19,35 @@ public struct ToastCatalog: View {
       .padding(Spacing.medium)
     }
     .background(Color.background(Color.BackgroundColor.light))
-    .navigationTitle("Toast")
-    .withToastHandling(toastView, position: toastPosition)
+    .navigationTitle("Fixed Confirmation Toast")
+    .withToastHandling(toastView, position: position)
   }
 
-  private func closeDialog() {
+  private func closeToast() {
     toastView = nil
   }
 
   private var defaultToast: some View {
     VStack(alignment: .leading) {
-      PBToast(text: "Error Message", variant: .error, actionView: .default, dismissAction: closeDialog)
-      PBToast(text: "Items Successfully Moved", variant: .success, dismissAction: closeDialog)
-      PBToast(text: "Scan to Assign Selected Items", variant: .neutral, dismissAction: closeDialog)
+      PBToast(text: "Error Message", variant: .error, dismissAction: closeToast)
+      PBToast(text: "Items Successfully Moved", variant: .success, dismissAction: closeToast)
+      PBToast(text: "Scan to Assign Selected Items", variant: .neutral, dismissAction: closeToast)
     }
   }
 
   private var multiLine: some View {
-    PBToast(text: message, variant: .custom(.infoCircle, .pbPrimary), dismissAction: closeDialog)
+    PBToast(text: message, variant: .custom(.infoCircle, .pbPrimary), dismissAction: closeToast)
   }
 
   private var clickToClose: some View {
     VStack(alignment: .leading) {
-      PBToast(text: "Error Message", variant: .error, actionView: .default, dismissAction: closeDialog)
-      PBToast(text: "Items Successfully Moved", variant: .success, actionView: .default, dismissAction: closeDialog)
-      PBToast(text: "Scan to Assign Selected Items", variant: .neutral, actionView: .default, dismissAction: closeDialog)
+      PBToast(text: "Error Message", variant: .error, actionView: .default, dismissAction: closeToast)
+      PBToast(text: "Items Successfully Moved", variant: .success, actionView: .default, dismissAction: closeToast)
+      PBToast(text: "Scan to Assign Selected Items", variant: .neutral, actionView: .default, dismissAction: closeToast)
     }
   }
 
   private var positionButton: some View {
-<<<<<<< HEAD
-    HStack {
-      PBButton(variant: .secondary, title: "Top Center") {
-        toastPosition = .top
-        toastView = PBToast(
-          text: "Top Center",
-          variant: .neutral,
-          actionView: .default,
-          dismissAction: closeDialog
-        )
-      }
-=======
-<<<<<<< HEAD
     return Grid {
       GridRow {
         PBButton(variant: .secondary, title: "Top Left") {
@@ -103,7 +83,7 @@ public struct ToastCatalog: View {
           position = .bottom
           toastView = PBToast(
             text: "Bottom Center",
-            variant: .custom(.user, .pbPrimary),
+            variant: .custom(.user, .status(.neutral)),
             actionView: .default,
             dismissAction: closeToast
           )
@@ -114,7 +94,7 @@ public struct ToastCatalog: View {
           position = .bottomLeft
           toastView = PBToast(
             text: "Bottom Left",
-            variant: .custom(.user, .pbPrimary),
+            variant: .custom(.user, .status(.neutral)),
             actionView: .default,
             dismissAction: closeToast
           )
@@ -123,38 +103,11 @@ public struct ToastCatalog: View {
           position = .bottomRight
           toastView = PBToast(
             text: "Bottom Right",
-            variant: .custom(.user, .pbPrimary),
+            variant: .custom(.user, .status(.neutral)),
             actionView: .default,
             dismissAction: closeToast
           )
         }
-=======
-    HStack {
-      PBButton(variant: .secondary, title: "Top Center") {
-        toastPosition = .top
-        toastView = PBToast(
-          text: "Top Center",
-          variant: .neutral,
-          actionView: .default,
-          dismissAction: closeToast
-        )
-      }
->>>>>>> 6f3c88b (fixed close example)
-
-      PBButton(variant: .secondary, title: "Bottom Center") {
-        toastPosition = .bottom
-        toastView = PBToast(
-          text: "Bottom Center",
-          variant: .custom(.user, .pbPrimary),
-          actionView: .default,
-<<<<<<< HEAD
-          dismissAction: closeDialog
-        )
-=======
-          dismissAction: closeToast
-        )
->>>>>>> 19b6c90 (fixed close example)
->>>>>>> 6f3c88b (fixed close example)
       }
     }
   }
@@ -165,7 +118,7 @@ public struct ToastCatalog: View {
         text: message,
         variant: .success,
         actionView: .custom(AnyView(Text("Undo").pbFont(.title4, color: .white))),
-        dismissAction: closeDialog
+        dismissAction: closeToast
       )
 
       PBToast(
@@ -177,7 +130,7 @@ public struct ToastCatalog: View {
               PBButton(variant: .primary, title: "Undo").disabled(true)
             }
           )),
-        dismissAction: closeDialog
+        dismissAction: closeToast
       )
     }
   }
@@ -185,22 +138,22 @@ public struct ToastCatalog: View {
   private var withTimer: some View {
     HStack {
       PBButton(variant: .secondary, title: "Top Center") {
-        toastPosition = .top
+        position = .top
         toastView = PBToast(
           text: "Top Center",
           variant: .neutral,
           actionView: .withTimer(3),
-          dismissAction: closeDialog
+          dismissAction: closeToast
         )
       }
 
       PBButton(variant: .secondary, title: "Bottom Center") {
-        toastPosition = .bottom
+        position = .bottom
         toastView = PBToast(
           text: "Bottom Center",
           variant: .neutral,
           actionView: .withTimer(2),
-          dismissAction: closeDialog
+          dismissAction: closeToast
         )
       }
     }
