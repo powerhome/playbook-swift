830dd509e236d745accae0405af250d53d1efdd7
RachelRadford21 <rachel.radford@powerhrg.com>
2023-11-08 12:19:24 -0500

    Date kit work in progress

---
+++ Sources/Playbook/Components/Date/DatestampCatalog.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Rachel Radford on 11/8/23.
+//
+
+import SwiftUI
+
+public struct DateStampCatalog: View {
+    public init() {}
+  public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateStampCatalog()
+}
---
+++ Sources/Playbook/Components/Date/PBDatestamp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Rachel Radford on 11/8/23.
+//
+
+import SwiftUI
+
+struct PBDatestamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PBDatestamp()
+}
