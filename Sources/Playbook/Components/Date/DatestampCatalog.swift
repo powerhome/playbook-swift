aa6778680642e7ceac090ddb99ecc8e032dd5f91
RachelRadford21 <rachel.radford@powerhrg.com>
2023-11-08 14:21:53 -0500

    Date kit finished

--- Sources/Playbook/Components/Date/DatestampCatalog.swift
+++ Sources/Playbook/Components/Date/DatestampCatalog.swift
@@ -1,19 +1,74 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Rachel Radford on 11/8/23.
 //
 
 import SwiftUI
 
-public struct DateStampCatalog: View {
-    public init() {}
+public struct DatestampCatalog: View {
+  public init() {}
+  let minWidth: CGFloat = 0
   public var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ScrollView {
+      VStack(spacing: Spacing.medium) {
+        PBDoc(title: "Default") {
+          defaultView
+        }
+        PBDoc(title: "Variants") {
+          variantView
+        }
+        PBDoc(title: "Alignment") {
+          alignmentView
+        }
+        PBDoc(title: "Unstyled") {
+          unstyledView
+        }
+      }
+      .padding(Spacing.medium)
     }
+    .background(Color.background(Color.BackgroundColor.light))
+    .navigationTitle("Date")
+  }
 }
 
-#Preview {
-    DateStampCatalog()
+public extension DatestampCatalog {
+  var defaultView: some View {
+    VStack(alignment: .leading, spacing: Spacing.small) {
+      ForEach(PBDatestamp.Variant.showCases, id: \.self) {  variant in
+        PBDatestamp(Date(), variant: variant)
+      }
+      Spacer()
+      ForEach(PBDatestamp.Variant.showCases, id: \.self) {  variant in
+        PBDatestamp(Date(), variant: variant, typography: .title4)
+      }
+    }
+  }
+
+  var variantView: some View {
+    VStack(alignment: .leading, spacing: Spacing.small) {
+      PBDatestamp(Date(), variant: .withIcon(isStandard: true), typography: .caption, iconSize: .small)
+      PBDatestamp(Date(), variant: .standard, typography: .title4)
+      PBDatestamp(Date(), variant: .withIcon(isStandard: true), typography: .title4, iconSize: .large)
+      PBDatestamp(Date(), variant: .dayDate, typography: .title4)
+      PBDatestamp(Date(), variant: .withIcon(isStandard: false), typography: .title4, iconSize: .large)
+    }
+  }
+
+  var alignmentView: some View {
+    VStack(spacing: Spacing.small) {
+      PBDatestamp(Date(), variant: .standard, typography: .title4, alignment: .leading)
+      PBDatestamp(Date(), variant: .withIcon(isStandard: true), typography: .title4, alignment: .center)
+      PBDatestamp(Date(), variant: .short, typography: .title4, alignment: .trailing)
+    }
+  }
+
+  var unstyledView: some View {
+    VStack(alignment: .leading, spacing: Spacing.small) {
+      PBDatestamp(Date(), variant: .short, typography: .body)
+      PBDatestamp(Date(), variant: .standard, typography: .title1)
+      PBDatestamp(Date(), variant: .withIcon(isStandard: false), typography: .subcaption)
+    }
+  }
 }
--- Sources/Playbook/Components/Date/PBDatestamp.swift
+++ Sources/Playbook/Components/Date/PBDatestamp.swift
@@ -1,18 +1,100 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Rachel Radford on 11/8/23.
 //
 
 import SwiftUI
 
-struct PBDatestamp: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct PBDatestamp: View {
+  let datestamp: Date
+  let variant: Variant
+  let typography: PBFont
+  let iconSize: PBIcon.IconSize
+  let alignment: Alignment
+  public init(
+    _ datestamp: Date,
+    variant: Variant = .short,
+    typography: PBFont = .caption,
+    iconSize: PBIcon.IconSize = .x1,
+    alignment: Alignment = .leading
+  ) {
+    self.datestamp = datestamp
+    self.variant = variant
+    self.typography = typography
+    self.iconSize = iconSize
+    self.alignment = alignment
+  }
+
+  public var body: some View {
+    HStack {
+      iconView
+      Text(datestampStyle)
+        .pbFont(typography)
     }
+    .frame(maxWidth: .infinity, alignment: alignment)
+  }
+
+  var formattedShortened: String {
+    let formatter  = DateFormatter()
+    formatter.dateFormat = "MMM d"
+    return formatter.string(from: datestamp)
+  }
+
+  var formattedHyphenated: String {
+    let formatter  = DateFormatter()
+    formatter.dateFormat = "MMM - d - YYYY"
+    return formatter.string(from: datestamp)
+  }
+
+  var formattedStandard: String {
+    let formatter  = DateFormatter()
+    formatter.dateFormat = "MMM d, YYYY"
+    return formatter.string(from: datestamp)
+  }
+
+  var formattedDayDate: String {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "EEE \u{00b7} MMM d, YYYY"
+    return formatter.string(from: datestamp)
+  }
+
+  var datestampStyle: String {
+    switch variant {
+    case .short: return formattedShortened
+    case .hyphenated: return formattedHyphenated
+    case .standard: return formattedStandard
+    case .dayDate: return formattedDayDate
+    case .withIcon(let isStandard): return isStandard ? formattedStandard : formattedDayDate
+    }
+  }
+
+  var iconView: AnyView? {
+    switch variant {
+    case .withIcon:
+      return AnyView(PBIcon.fontAwesome(.calendar, size: iconSize))
+    default:
+      return nil
+    }
+  }
 }
 
-#Preview {
-    PBDatestamp()
+public extension PBDatestamp {
+  enum Variant: CaseIterable, Hashable {
+    public static var allCases: [PBDatestamp.Variant] {
+      return [.short, .dayDate, .hyphenated, .standard, .withIcon(isStandard: true), .withIcon(isStandard: false)]
+    }
+    public static var showCases: [PBDatestamp.Variant] {
+      return [.short, .hyphenated, .standard, .dayDate]
+    }
+    case short, hyphenated, standard, dayDate, withIcon(isStandard: Bool)
+  }
+}
+
+public struct PBDatetamp_Previews: PreviewProvider {
+  public static var previews: some View {
+    registerFonts()
+    return DatestampCatalog()
+  }
 }
