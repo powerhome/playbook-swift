6332be935ac7656b84c2e404093f18117f0bcded
RachelRadford21 <rachelaradford21@gmail.com>
2023-12-11 11:30:49 -0500

    Changed force unwrapping date extension

--- Sources/Playbook/Resources/Extensions/Extension+Date.swift
+++ Sources/Playbook/Resources/Extensions/Extension+Date.swift
@@ -11,6 +11,6 @@ extension Date {
   func makeDate(year: Int = 0, month: Int, day: Int, hr: Int = 0, min: Int = 0, sec: Int = 0) -> Date {
     let calendar = Calendar(identifier: .gregorian)
     let components = DateComponents(year: year, month: month, day: day, hour: hr, minute: min, second: sec)
-    return calendar.date(from: components)!
+    return calendar.date(from: components) ?? Date()
   }
 }
