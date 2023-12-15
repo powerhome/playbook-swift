3e084fd4f7337974e27ffbefa64d45d608cb1199
RachelRadford21 <rachelaradford21@gmail.com>
2023-12-15 09:34:50 -0500

    moved font awesome icons to the correct place alphabetically

--- Sources/Playbook/Design Elements/Iconography/FontAwesome.swift
+++ Sources/Playbook/Design Elements/Iconography/FontAwesome.swift
@@ -2261,6 +2261,7 @@ public enum FontAwesome: String, PlaybookGenericIcon, CaseIterable {
     case .laptopCode: return "\u{f5fc}"
     case .laptopHouse: return "\u{e066}"
     case .laptopMedical: return "\u{f812}"
+    case .laptopMobile: return "\u{f87a}"
     case .laravel: return "\u{f3bd}"
     case .lastfm: return "\u{f202}"
     case .lastfmSquare: return "\u{f203}"
@@ -2451,6 +2452,7 @@ public enum FontAwesome: String, PlaybookGenericIcon, CaseIterable {
     case .phoenixSquadron: return "\u{f511}"
     case .phone: return "\u{f095}"
     case .phoneAlt: return "\u{f879}"
+    case .phoneOffice: return "\u{f67d}"
     case .phonePlus: return "\u{f4d2}"
     case .phoneSlash: return "\u{f3dd}"
     case .phoneSquare: return "\u{f098}"
@@ -2970,8 +2972,6 @@ public enum FontAwesome: String, PlaybookGenericIcon, CaseIterable {
     case .youtube: return "\u{f167}"
     case .youtubeSquare: return "\u{f431}"
     case .zhihu: return "\u{f63f}"
-    case .laptopMobile: return "\u{f87a}"
-    case .phoneOffice: return "\u{f67d}"
     }
   }
 }
