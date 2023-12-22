54ff3455fda9d1362119ea37454e2c43fdd8420f
RachelRadford21 <rachelaradford21@gmail.com>
2023-12-22 09:17:13 -0500

    removed "Wrong Number" Text

--- Sources/Playbook/Components/Person Contact/PersonContactCatalog.swift
+++ Sources/Playbook/Components/Person Contact/PersonContactCatalog.swift
@@ -63,10 +63,9 @@ public extension PersonContactCatalog {
   var withWrongNumbersView: some View {
     VStack(alignment: .leading, spacing: Spacing.xSmall) {
       PBPersonContact(firstName: "Pauline", lastName: "Smith", contacts: [PBContact(type: .email, value: "email@example.com", detail: false), PBContact(type: .home, value: " (555) 555-5555", detail: false), PBContact(type: .home, value: "(304) 861-5385", detail: false)])
-    Spacer()
 
-      PBPersonContact(firstName: "Wrong", lastName: "Number", contacts: [PBContact(type: .custom("", .phoneSlash), value: "(324) 562-7482", detail: false)])
-        .padding(.top, -10)
+      PBPersonContact(firstName: "", lastName: "", contacts: [PBContact(type: .custom("", .phoneSlash), value: "(324) 562-7482", detail: false)])
+       
         
     }
    
