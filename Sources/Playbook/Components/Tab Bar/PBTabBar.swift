5cdd62b4217ad74f37b6c41ac186bafa75349989
RachelRadford21 <rachelaradford21@gmail.com>
2024-01-01 09:13:41 -0500

    Fixed State var highlighting tabs individually and not all tabs

--- Sources/Playbook/Components/Tab Bar/PBTabBar.swift
+++ Sources/Playbook/Components/Tab Bar/PBTabBar.swift
@@ -8,20 +8,19 @@
 import SwiftUI
 
 public struct PBTabBar: View {
-  @State var isTabSelected: Bool = false
-  var tabIconCount: Int = 0
+  @Binding var selectedTab: Int
   let hasBorder: Bool?
   let hasShadow: Bool?
-  var icon: [PBIcon]
-  var iconName: FontAwesome
+  var icon: [FontAwesome]
+  var iconName: [String]
   public init(
-    tabIconCount: Int = 0,
+    selectedTab: Binding<Int> = .constant(0),
     hasBorder: Bool = false,
     hasShadow: Bool = false,
-    icon: [PBIcon] = [PBIcon.fontAwesome(.home, size: .large)],
-    iconName: FontAwesome = .home
+    icon: [FontAwesome] = [(.home)],
+    iconName: [String] = [""]
   ) {
-    self.tabIconCount = tabIconCount
+    self._selectedTab = selectedTab
     self.hasBorder = hasBorder
     self.hasShadow = hasShadow
     self.icon = icon
@@ -30,46 +29,33 @@ public struct PBTabBar: View {
   
   public var body: some View {
     PBCard(alignment: .center, backgroundColor: Color.card, border: hasBorder ?? false, style: PBCardStyle.default, shadow: hasShadow ?? false ? .deep : Shadow.none) {
-      HStack(spacing: Spacing.xLarge) {
-        ForEach(0..<tabIconCount, id: \.description) { _ in
-          tabButtonView
-            .frame(maxWidth: .infinity)
-            .foregroundStyle(tabIconColor)
-        }
+      HStack {
+        tabButtonView
       }
-      .frame(maxWidth: .infinity, alignment: .bottom)
-      .frame(height: 48)
     }
   }
 }
 
 public extension PBTabBar {
   var tabButtonView: some View {
-    return Button {
-      isTabSelected.toggle()
-      
-    } label: {
-      tabButtonLabelView
+    return ForEach(Array(zip(icon, iconName)), id: \.self.0) { (image, name) in
+      Spacer()
+      Button {
+        selectedTab = image.hashValue
+      } label: {
+          VStack(spacing: Spacing.xxSmall) {
+            PBIcon(image, size: .large)
+            Text(name)
+          }
+          .pbFont(.subcaption, color: selectedTab == image.hashValue ? Color.pbPrimary : Color.text(.light)
+          ).padding(.horizontal, -5)
+      }
+      .buttonStyle(.plain)
+      .padding(.bottom, Spacing.large)
+      .frame(maxWidth: .infinity, alignment: .bottom)
+      .frame(height: 48)
+      Spacer()
     }
-    .buttonStyle(.plain)
-    .pbFont(.subcaption, color: tabIconColor)
-    .padding(.bottom, Spacing.large)
-  }
-  @ViewBuilder
-  var tabButtonLabelView: some View {
-    VStack(spacing: Spacing.xxSmall) {
-      tabIconView
-      Text(tabIconNameView)
-    }
-  }
-  var tabIconView: PBIcon {
-    return PBIcon(iconName, size: .large)
-  }
-  var tabIconNameView: String {
-    return iconName.rawValue.replacingOccurrences(of: "fa-", with: "")
-  }
-  var tabIconColor: Color {
-    return isTabSelected == true ? Color.pbPrimary : Color.text(.light)
   }
 }
 
--- Sources/Playbook/Components/Tab Bar/TabBarCatalog.swift
+++ Sources/Playbook/Components/Tab Bar/TabBarCatalog.swift
@@ -8,7 +8,11 @@
 import SwiftUI
 
 public struct TabBarCatalog: View {
- 
+  @State var selectedTab: Int = 0
+  @State var selectedTab1: Int = 0
+  @State var selectedTab2: Int = 0
+  @State var selectedTab3: Int = 0
+  @State var selectedTab4: Int = 0
   public var body: some View {
     ScrollView {
       VStack(spacing: Spacing.small) {
@@ -16,38 +20,36 @@ public struct TabBarCatalog: View {
           .pbFont(.caption, variant: .light, color: .text(.light))
           .padding(.trailing, 250)
           .padding(.top, 30)
-//        PBCard(alignment: .center, backgroundColor: Color.card, border: true, style: PBCardStyle.default, shadow: .deep) {
+
           dropShadow
        
-      //  }
         Text("Without Shadow")
           .pbFont(.caption, variant: .light, color: .text(.light))
           .padding(.trailing, 220)
           .padding(.top, 10)
-        PBCard(alignment: .center, backgroundColor: Color.card, border: false, style: PBCardStyle.default) {
+       
           withoutShadow
-        }
+        
         Text("With Border")
           .pbFont(.caption, variant: .light, color: .text(.light))
           .padding(.trailing, 250)
           .padding(.top, 10)
-        PBCard(alignment: .center, backgroundColor: Color.card, border: true, style: PBCardStyle.default) {
+      
           withBorder
-        }
+        
         Text("4 options")
           .pbFont(.caption, variant: .light, color: .text(.light))
           .padding(.trailing, 250)
           .padding(.top, 10)
-        PBCard(alignment: .center, backgroundColor: Color.card, border: true, style: PBCardStyle.default, shadow: .deep) {
-          fourOptions
-        }
+        
+         fourOptions
+        
         Text("3 options")
           .pbFont(.caption, variant: .light, color: .text(.light))
           .padding(.trailing, 250)
           .padding(.top, 10)
-        PBCard(alignment: .center, backgroundColor: Color.card, border: true, style: PBCardStyle.default, shadow: .deep) {
+       
           threeOptions
-        }
       }
     }
     .background(Color.background(Color.BackgroundColor.light))
@@ -57,52 +59,58 @@ public struct TabBarCatalog: View {
 
 public extension TabBarCatalog {
   var dropShadow: some View {
-    return HStack(spacing: Spacing.medium) {
-//      PBTabBar(hasBorder: true, hasShadow: true, icon: [PBIcon.fontAwesome(.home, size: .large), PBIcon.fontAwesome(.home, size: .large)])
-      PBTabBar(tabIconCount: 3, hasBorder: true, hasShadow: true, iconName: .home)
-    //  PBTabBar(hasBorder: true, hasShadow: true, iconName: .home)
-//      PBTabBar(isTabSelected: true, variant: .home)
-//      PBTabBar(variant: .calendar)
-//      PBTabBar(variant: .notifications)
-//      PBTabBar(variant: .search)
-//      PBTabBar(variant: .more)
+    return HStack {
+      PBTabBar(
+        selectedTab: $selectedTab,
+        hasBorder: false,
+        hasShadow: true,
+        icon: [.home, .calendar, .bell, .search, .ellipsisH],
+        iconName: ["home", "calendar", "notfications", "search", "more"]
+      )
     }
   }
   var withoutShadow: some View {
-    return HStack(spacing: Spacing.medium) {
-      Text("")
-//      PBTabBar(isTabSelected: true, variant: .home)
-//      PBTabBar(variant: .calendar)
-//      PBTabBar(variant: .notifications)
-//      PBTabBar(variant: .search)
-//      PBTabBar(variant: .more)
+    return HStack {
+      PBTabBar(
+        selectedTab: $selectedTab1,
+        hasBorder: false,
+        hasShadow: false,
+        icon: [.home, .calendar, .bell, .search, .ellipsisH],
+        iconName: ["home", "calendar", "notfications", "search", "more"]
+      )
     }
   }
   var withBorder: some View {
-    return HStack(spacing: Spacing.medium) {
-      Text("")
-//      PBTabBar(isTabSelected: true, variant: .home)
-//      PBTabBar(variant: .calendar)
-//      PBTabBar(variant: .notifications)
-//      PBTabBar(variant: .search)
-//      PBTabBar(variant: .more)
+    return HStack {
+      PBTabBar(
+        selectedTab: $selectedTab2,
+        hasBorder: true,
+        hasShadow: false,
+        icon: [.home, .calendar, .bell, .search, .ellipsisH],
+        iconName: ["home", "calendar", "notfications", "search", "more"]
+      )
     }
   }
   var fourOptions: some View {
     return HStack(spacing: Spacing.large) {
-      Text("")
-//      PBTabBar(isTabSelected: true, variant: .home)
-//      PBTabBar(variant: .calendar)
-//      PBTabBar(variant: .notifications)
-//      PBTabBar(variant: .search)
+      PBTabBar(
+        selectedTab: $selectedTab3,
+        hasBorder: false,
+        hasShadow: true,
+        icon: [.home, .calendar, .bell, .search],
+        iconName: ["home", "calendar", "notfications", "search"]
+      )
     }
   }
   var threeOptions: some View {
-    return HStack(spacing: Spacing.xLarge) {
-      Text("")
-//      PBTabBar(isTabSelected: true, variant: .home)
-//      PBTabBar(variant: .notifications)
-//      PBTabBar(variant: .search)
+    return HStack {
+      PBTabBar(
+        selectedTab: $selectedTab4,
+        hasBorder: false,
+        hasShadow: true,
+        icon: [.home, .bell, .search],
+        iconName: ["home", "notfications", "search"]
+      )
     }
   }
 }
