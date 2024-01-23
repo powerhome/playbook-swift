3fc36b52cebdbcdba53663b487ecf84bc30eb0f8
isis.silva <isisms2602@gmail.com>
2024-01-23 18:02:59 -0500

    add beta target

---
+++ PlaybookShowcase/PlaybookShowcase-iOS-beta/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  Playbook Swift Design System
+//
+//  Copyright © 2024 Power Home Remodeling Group
+//  This software is distributed under the ISC License
+//
+//  ContentView.swift
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
