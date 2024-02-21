518794b0003e6400e2689f1685d9fc3401999e62
RachelRadford21 <54749071+RachelRadford21@users.noreply.github.com>
2024-02-21 10:42:50 -0500

    [PBIOS-278] NEW Highlight kit (#283)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    [PBIOS-278] NEW Highlight kit
    
    **Screenshots:** Screenshots to visualize your addition/change
    ![Simulator Screenshot - iPhone 15 Pro Max - 2024-02-16 at 13 42
    07](https://github.com/powerhome/PlaybookSwift/assets/54749071/85807a16-94ef-474c-bbd4-be1637a9d1fe)
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    ### Checklist
    - [x] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [x] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [x] **TESTING** - Have you tested your story?
    
    ---------
    
    Co-authored-by: isis.silva <isisms2602@gmail.com>
    Co-authored-by: Ísis <isis.silva@powerhrg.com>

---
+++ Sources/Playbook/Components/Highlight/PBHighlight.swift
@@ -0,0 +1,40 @@
+//
+//  Playbook Swift Design System
+//
+//  Copyright © 2024 Power Home Remodeling Group
+//  This software is distributed under the ISC License
+//
+//  PBHighlight.swift
+//
+
+import SwiftUI
+
+public struct PBHighlight: View {
+  let text: String
+  let highlightedText: [String]
+  public init(
+    text: String = "",
+    highlightedText: [String] = []
+  ) {
+    self.text = text
+    self.highlightedText = highlightedText
+  }
+  public var body: some View {
+    emphasizedText
+  }
+}
+
+extension PBHighlight {
+  var emphasizedText: some View {
+    return Text(textToHighlight)
+      .pbFont(.body, variant: .light, color: .text(.default))
+  }
+  var textToHighlight: AttributedString {
+    return highlightedAttributedText(text: text, highlightedText: highlightedText)
+  }
+}
+
+#Preview {
+  registerFonts()
+  return HighlightCatalog()
+}
