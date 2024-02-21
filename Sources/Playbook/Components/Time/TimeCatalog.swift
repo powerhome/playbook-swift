98475d703a6f7c248f6b0eb6564b9c09ec601056
RachelRadford21 <rachelaradford21@gmail.com>
2024-02-21 11:30:41 -0500

    formatting commit

--- Sources/Playbook/Components/Time/TimeCatalog.swift
+++ Sources/Playbook/Components/Time/TimeCatalog.swift
@@ -39,36 +39,46 @@ struct TimeCatalog: View {
 extension TimeCatalog {
   var defaultView: some View {
     VStack(alignment: .leading, spacing: Spacing.small) {
-      PBTime(variant: .time)
-      PBTime(showTimeZone: true,
-             variant: .iconTimeZone
+      PBTime(
+        variant: .time
       )
-      PBTime(showIcon: true,
-             variant: .iconTimeZone
+      PBTime(
+        showTimeZone: true,
+        variant: .iconTimeZone
       )
-      PBTime(variant: .iconTimeZone)
+      PBTime(
+        showIcon: true,
+        variant: .iconTimeZone
+      )
+      PBTime(
+        variant: .iconTimeZone
+      )
       Spacer()
-      PBTime(variant: .time,
-             isLowercase: true,
-             isBold: true,
-             unstyled: .body
-             
+      PBTime(
+        variant: .time,
+        isLowercase: true,
+        isBold: true,
+        unstyled: .body
+        
       )
-      PBTime(showTimeZone: true,
-             variant: .iconTimeZone,
-             isLowercase: true,
-             isBold: true,
-             unstyled: .body
+      PBTime(
+        showTimeZone: true,
+        variant: .iconTimeZone,
+        isLowercase: true,
+        isBold: true,
+        unstyled: .body
       )
-      PBTime(showIcon: true,
-             variant: .iconTimeZone,
-             isLowercase: true,
-             isBold: true
+      PBTime(
+        showIcon: true,
+        variant: .iconTimeZone,
+        isLowercase: true,
+        isBold: true
       )
-      PBTime(variant: .iconTimeZone,
-             isLowercase: true,
-             isBold: true,
-             unstyled: .body
+      PBTime(
+        variant: .iconTimeZone,
+        isLowercase: true,
+        isBold: true,
+        unstyled: .body
       )
     }
   }
@@ -86,36 +96,41 @@ extension TimeCatalog {
   }
   var handlingTimeZones: some View {
     VStack(alignment: .leading, spacing: Spacing.small) {
-      PBTime(showTimeZone: true,
-             variant: .withTimeZoneHeader,
-             isLowercase: true,
-             header: "East Coast",
-             isBold: true,
-             unstyled: .body
+      PBTime(
+        showTimeZone: true,
+        variant: .withTimeZoneHeader,
+        isLowercase: true,
+        header: "East Coast",
+        isBold: true,
+        unstyled: .body
       )
-      PBTime(showTimeZone: true, 
-             variant: .withTimeZoneHeader,
-             header: "Central",
-             zone: .central,
-             timeIdentifier: "CST"
+      PBTime(
+        showTimeZone: true,
+        variant: .withTimeZoneHeader,
+        header: "Central",
+        zone: .central,
+        timeIdentifier: "CST"
       )
-      PBTime(showTimeZone: true,
-             variant: .withTimeZoneHeader,
-             header: "Mountain",
-             zone: .mountain,
-             timeIdentifier: "MST"
+      PBTime(
+        showTimeZone: true,
+        variant: .withTimeZoneHeader,
+        header: "Mountain",
+        zone: .mountain,
+        timeIdentifier: "MST"
       )
-      PBTime(showTimeZone: true,
-             variant: .withTimeZoneHeader,
-             header: "West Coast",
-             zone: .pacific,
-             timeIdentifier: "PST"
+      PBTime(
+        showTimeZone: true,
+        variant: .withTimeZoneHeader,
+        header: "West Coast",
+        zone: .pacific,
+        timeIdentifier: "PST"
       )
-      PBTime(showTimeZone: true,
-             variant: .withTimeZoneHeader,
-             header: "Tokyo, Japan",
-             zone: .gmt,
-             timeIdentifier: "GMT+9"
+      PBTime(
+        showTimeZone: true,
+        variant: .withTimeZoneHeader,
+        header: "Tokyo, Japan",
+        zone: .gmt,
+        timeIdentifier: "GMT+9"
       )
     }
   }
@@ -129,7 +144,8 @@ extension TimeCatalog {
         alignment: .leading,
         unstyled: .body
       )
-      PBTime(showTimeZone: true,
+      PBTime(
+        showTimeZone: true,
         variant: .iconTimeZone,
         isLowercase: true,
         isBold: true,
