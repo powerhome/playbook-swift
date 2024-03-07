b01e7d97ecdaddf9caa00fffdea993c7a1bd6336
RachelRadford21 <54749071+RachelRadford21@users.noreply.github.com>
2024-03-07 10:58:43 -0500

    [PBIOS-294] Time Kit function rework (#294)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    [PBIOS-294] Time Kit function rework
    
    **Screenshots:** Screenshots to visualize your addition/change
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    ### Checklist
    - [x] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [x] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [x] **TESTING** - Have you tested your story?
    
    ---------
    
    Co-authored-by: isis.silva <isisms2602@gmail.com>
    Co-authored-by: Ísis <isis.silva@powerhrg.com>

--- Sources/Playbook/Components/Time/PBTime.swift
+++ Sources/Playbook/Components/Time/PBTime.swift
@@ -55,9 +55,9 @@ public struct PBTime: View {
     self.unstyled = unstyled
     self.timeIdentifier = timeIdentifier
   }
-    public var body: some View {
-     timeVariants
-    }
+  public var body: some View {
+    timeVariants
+  }
 }
 
 public extension PBTime {
@@ -67,7 +67,7 @@ public extension PBTime {
     case withTimeZoneHeader
   }
   enum Zones {
-    case east, central, mountain, pacific, gmt
+    case east, central, mountain, pacific, gmt, utc
   }
   @ViewBuilder
   var timeVariants: some View {
@@ -87,13 +87,15 @@ public extension PBTime {
   }
   @ViewBuilder
   var time: some View {
-   Text(getTime(timeZoneIdentifier: timeIdentifier))
+    Text(getTime(timeZoneIdentifier: timeIdentifier))
       .pbFont(unstyled, variant: isBold ? .bold : .light, color: textColor)
   }
   @ViewBuilder
   var timeZone: some View {
     switch zone {
     case .east, .central, .mountain, .pacific, .gmt: Text(timeIdentifier)
+    case .utc: Text("")
+
     }
   }
   var showIconView: some View {
@@ -117,17 +119,17 @@ public extension PBTime {
     }
   }
   var iconTimeZone: some View {
-   return HStack {
-    if showIcon {
-      showIconView
+    return HStack {
+      if showIcon {
+        showIconView
       } else if showTimeZone {
         showTimeZoneView
       } else {
         showIconTimeZoneView
       }
     }
-   .pbFont(unstyled, variant: isTimeZoneBold ? .bold : .light, color: isTimeZoneBold ? .text(.default) : .text(.light))
-   .frame(maxWidth: .infinity, alignment: alignment)
+    .pbFont(unstyled, variant: isTimeZoneBold ? .bold : .light, color: isTimeZoneBold ? .text(.default) : .text(.light))
+    .frame(maxWidth: .infinity, alignment: alignment)
   }
   var withTimeZoneHeader: some View {
     return VStack(alignment: .leading, spacing: Spacing.xSmall) {
@@ -137,16 +139,25 @@ public extension PBTime {
     .pbFont(unstyled, variant: isTimeZoneBold ? .bold : .light, color: isTimeZoneBold ? .text(.default) : .text(.light))
   }
   var textColor: Color {
-  isBold ? .text(.default) : .text(.light)
+    isBold ? .text(.default) : .text(.light)
   }
   func getTime(timeZoneIdentifier: String) -> String {
-    if let timeZone = TimeZone(abbreviation: timeZoneIdentifier) {
-      let formatter = DateFormatter()
+    let formatter = DateFormatter()
+    if let timeZone = TimeZone(identifier: timeZoneIdentifier) {
       formatter.timeZone = timeZone
       formatter.dateFormat = "h:mma"
       formatter.amSymbol = isLowercase == true ? "a" : "A"
       formatter.pmSymbol = isLowercase == true ? "p" : "P"
       return formatter.string(from: date ?? Date())
+    }
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
+    if let timeZone = formatter.date(from: timeZoneIdentifier) {
+      let timeFormatter = DateFormatter()
+      timeFormatter.dateFormat = "h:mma z"
+      timeFormatter.amSymbol = isLowercase == true ? "a" : "A"
+      timeFormatter.pmSymbol = isLowercase == true ? "p" : "P"
+      let formattedString = timeFormatter.string(from: timeZone)
+      return formattedString
     }
     return timeZoneIdentifier
   }
