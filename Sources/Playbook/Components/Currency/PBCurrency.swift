1239d276ff6914dbaa1338a29d9dd2bdca15e204
RachelRadford21 <54749071+RachelRadford21@users.noreply.github.com>
2024-03-14 16:08:38 -0400

    [PBIOS-291] Currency Kit (#293)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    [PBIOS-291] Currency Kit
    
    **Screenshots:** Screenshots to visualize your addition/change
    ![Simulator Screenshot - iPhone 15 Pro Max - 2024-03-01 at 14 00
    03](https://github.com/powerhome/playbook-swift/assets/54749071/3b5ec87a-0101-43b2-a29d-948bf7e85fd0)
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    ### Checklist
    - [x] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [x] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [x] **TESTING** - Have you tested your story?
    
    ---------
    
    Co-authored-by: Ísis <isis.silva@powerhrg.com>

---
+++ Sources/Playbook/Components/Currency/PBCurrency.swift
@@ -0,0 +1,92 @@
+//
+//  Playbook Swift Design System
+//
+//  Copyright © 2024 Power Home Remodeling Group
+//  This software is distributed under the ISC License
+//
+//  PBCurrency.swift
+//
+
+import SwiftUI
+
+public struct PBCurrency: View {
+  let amount: String?
+  let decimalAmount: String?
+  let label: String?
+  let size: PBFont
+  let symbol: String?
+  let unit: String?
+  let isEmphasized: Bool
+  let hasUnit: Bool
+  public init(
+    amount: String? = nil,
+    decimalAmount: String? = nil,
+    label: String? = nil,
+    size: PBFont = .body,
+    symbol: String? = nil,
+    unit: String? = nil,
+    isEmphasized: Bool = false,
+    hasUnit: Bool = false
+  ) {
+    self.amount = amount
+    self.decimalAmount = decimalAmount
+    self.label = label
+    self.size = size
+    self.symbol = symbol
+    self.unit = unit
+    self.isEmphasized = isEmphasized
+    self.hasUnit = hasUnit
+  }
+  public var body: some View {
+    currencyView
+  }
+}
+public extension PBCurrency {
+  var currencyView: some View {
+    VStack(alignment: .leading, spacing: Spacing.xSmall) {
+        labelView
+      HStack(spacing: 0) {
+        symbolView
+        dollarDecimalView
+      }
+    }
+    
+  }
+  var labelView: some View {
+      Text(label ?? "")
+        .pbFont(.caption, variant: .bold, color: .text(.light))
+  }
+  var symbolView: some View {
+    Text(getCurrency(symbol: symbol ?? ""))
+      .pbFont(.body, color: .text(.light))
+      .baselineOffset(size == .title4  ? 0 : size == .title1 ? 21 : 9)
+  }
+  @ViewBuilder
+  var dollarDecimalView: some View {
+    HStack(spacing: 0) {
+      Text(formattedDollar)
+        .pbFont(size, variant: .bold)
+      Text(formattedDecimal)
+        .pbFont(.body, variant: .bold)
+        .baselineOffset(size == .title4 ? 0 : size == .title1 ? -16 : -5)
+    }
+  }
+  var formattedDollar: AttributedString {
+    return colorAttributedText(amount ?? "", characterToChange: amount ?? "", color: fontColor)
+  }
+  var formattedDecimal: AttributedString {
+    return colorAttributedText(hasUnit ? unit ?? "" : decimalAmount ?? "", characterToChange: hasUnit ? unit ?? "" : decimalAmount ?? "", color: .text(.light))
+  }
+  var fontColor: Color {
+    isEmphasized ? .text(.default) : .text(.light)
+  }
+  func getCurrency(symbol: String) -> String {
+    let locale = Locale(identifier: symbol)
+    let currencySymbol = locale.currencySymbol ?? ""
+    return currencySymbol
+  }
+}
+#Preview {
+  registerFonts()
+  return  CurrencyCatalog()
+}
