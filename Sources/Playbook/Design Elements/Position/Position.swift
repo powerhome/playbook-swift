a811dafccf178ed8310da7360e8779a496f62f1d
isis.silva <isisms2602@gmail.com>
2024-03-26 13:18:00 -0400

    add overlay to view modifier

---
+++ Sources/Playbook/Design Elements/Position/Position.swift
@@ -0,0 +1,66 @@
+//
+//  Playbook Swift Design System
+//
+//  Copyright © 2024 Power Home Remodeling Group
+//  This software is distributed under the ISC License
+//
+//  Position.swift
+//
+
+import SwiftUI
+
+public struct Position<T: View>: ViewModifier {
+  var top: CGFloat
+  var left: CGFloat
+  var right: CGFloat
+  var bottom: CGFloat
+  
+  var view: () -> T
+  
+  public init(
+    top: CGFloat = 0,
+    left: CGFloat = 0,
+    right: CGFloat = 0,
+    bottom: CGFloat = 0,
+    view: @escaping () -> T
+  ){
+    self.top = top
+    self.left = left
+    self.right = right
+    self.bottom = bottom
+    self.view = view
+  }
+  
+  public func body(content: Content) -> some View {
+    content
+      .overlay {
+        view()
+          .offset(x: left - right, y: top - bottom)
+      }
+  }
+}
+
+extension View {
+  func position<T: View>(
+    top: CGFloat = 0,
+    left: CGFloat = 0,
+    bottom: CGFloat = 0,
+    right: CGFloat = 0,
+    view: @escaping (() -> T)
+  ) -> some View{
+    self.modifier(
+      Position(
+        top: top,
+        left: left,
+        right: right,
+        bottom: bottom,
+        view: view
+      )
+    )
+  }
+}
+
+#Preview {
+  registerFonts()
+  return PositionCatalog()
+}
--- Sources/Playbook/Design Elements/Position/PositionCatalog.swift
+++ Sources/Playbook/Design Elements/Position/PositionCatalog.swift
@@ -11,12 +11,11 @@ import SwiftUI
 
 public struct PositionCatalog: View {
   public var body: some View {
-    VStack(alignment: .leading) {
+    VStack(alignment: .leading, spacing: 0) {
       PBAvatar(image: Image("andrew", bundle: .module), size: .large)
-        .overlay {
+        .position(top: 35, left: 0, bottom: 0, right: 0) {
           PBBadge(text: "On Roadtrip", rounded: true, variant: .neutral)
             .background(Color.card)
-            .positioning(top: 35, left: 0, bottom: 0, right: 0)
         }
     }
     .navigationTitle("Position")
