a5199dce710c186be202063d14728d779974c8bc
RachelRadford21 <rachelaradford21@gmail.com>
2024-04-02 15:54:33 -0400

    small code change

--- Sources/Playbook/Components/Loader/PBLoader.swift
+++ Sources/Playbook/Components/Loader/PBLoader.swift
@@ -12,7 +12,7 @@ import SwiftUI
 struct PBLoader: View {
   @State private var rotationAngle: Double
   @State private var dotIndex: Int
-  @State private var contentSize: CGSize = .zero
+  @State private var contentSize: CGSize
   let dotsCount: Int
   let dotSize: CGFloat
   let circleRadius: CGFloat
@@ -23,6 +23,7 @@ struct PBLoader: View {
   public init(
     rotationAngle: Double = 0,
     dotIndex: Int = 0,
+    contentSize: CGSize = .zero,
     dotsCount: Int = 7,
     dotSize: CGFloat = 2,
     circleRadius: CGFloat = 7,
@@ -33,6 +34,7 @@ struct PBLoader: View {
   ) {
     self.rotationAngle = rotationAngle
     self.dotIndex = dotIndex
+    self.contentSize = contentSize
     self.dotsCount = dotsCount
     self.dotSize = dotSize
     self.circleRadius = circleRadius
