f0f8aed31f8b29f1492f4a53e977b351c2cc07ac
isis.silva <isisms2602@gmail.com>
2024-05-02 16:20:49 -0400

    revert breaking changes

--- Sources/Playbook/Resources/Extensions/OnChange+Debounce.swift
+++ Sources/Playbook/Resources/Extensions/OnChange+Debounce.swift
@@ -73,3 +73,51 @@ extension Task {
     }
   }
 }
+
+struct DebouncingTaskViewModifier<ID: Equatable>: ViewModifier {
+  let id: ID
+  let priority: TaskPriority
+  let nanoseconds: UInt64
+  let task: @Sendable () async -> Void
+
+  init(
+    id: ID,
+    priority: TaskPriority = .high,
+    nanoseconds: UInt64 = 0,
+    task: @Sendable @escaping () async -> Void
+  ) {
+    self.id = id
+    self.priority = priority
+    self.nanoseconds = nanoseconds
+    self.task = task
+  }
+
+  func body(content: Content) -> some View {
+    content.task(id: id, priority: priority) {
+      do {
+        try await Task.sleep(nanoseconds: nanoseconds)
+        await task()
+      } catch {
+        // Ignore cancellation
+      }
+    }
+  }
+}
+
+extension View {
+  func task<ID: Equatable>(
+    id: ID,
+    priority: TaskPriority = .userInitiated,
+    nanoseconds: UInt64 = 0,
+    task: @Sendable @escaping () async -> Void
+  ) -> some View {
+    modifier(
+      DebouncingTaskViewModifier(
+        id: id,
+        priority: priority,
+        nanoseconds: nanoseconds,
+        task: task
+      )
+    )
+  }
+}
