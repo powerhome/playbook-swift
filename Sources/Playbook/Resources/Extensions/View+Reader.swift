bfb5f7ecbe6f52379b9b356718339addf01224f6
Ísis <isis.silva@powerhrg.com>
2024-05-07 03:16:18 +0700

    [PBIOS-270] Typeahead caret indicator float dropdown (#364)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    
    
    **Screenshots:** Screenshots to visualize your addition/change
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    ### Checklist
    - [ ] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [ ] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [ ] **TESTING** - Have you tested your story?
    
    ---------
    
    Co-authored-by: Stephen Marshall <stephen.marshall@powerhrg.com>

---
+++ Sources/Playbook/Resources/Extensions/View+Reader.swift
@@ -0,0 +1,65 @@
+//
+//  Playbook Swift Design System
+//
+//  Copyright © 2024 Power Home Remodeling Group
+//  This software is distributed under the ISC License
+//
+//  View+Reader.swift
+//
+
+import SwiftUI
+
+public extension View {
+  func coordinateSpace(in geometry: GeometryProxy) -> CGRect {
+    if #available(iOS 17.0, *), #available(macOS 14.0, *) {
+      return geometry.frame(in: .scrollView)
+    } else {
+      return geometry.frame(in: .global)
+    }
+  }
+
+  func frameReader(isPresented: Bool, in rect: @escaping (CGRect) -> Void) -> some View {
+    return background(
+      GeometryReader { geometry in
+        Color.clear
+          .onChange(of: isPresented) { _ in
+            rect(coordinateSpace(in: geometry))
+          }
+          .onAppear {
+            rect(coordinateSpace(in: geometry))
+          }
+      }
+        .hidden()
+    )
+  }
+  
+  func sizeReader(transaction: Transaction? = nil, size: @escaping (CGSize) -> Void) -> some View {
+    return background(
+      GeometryReader { geometry in
+        Color.clear
+          .preference(key: ContentSizeReaderPreferenceKey.self, value: geometry.size)
+          .onPreferenceChange(ContentSizeReaderPreferenceKey.self) { newValue in
+            DispatchQueue.main.async {
+              size(newValue)
+            }
+          }
+          .onChange(of: transaction?.animation) { _ in
+            DispatchQueue.main.async {
+              size(geometry.size)
+            }
+          }
+      }
+        .hidden()
+    )
+  }
+}
+
+struct ContentFrameReaderPreferenceKey: PreferenceKey {
+  static var defaultValue: CGRect { return CGRect() }
+  static func reduce(value: inout CGRect, nextValue: () -> CGRect) { value = nextValue() }
+}
+
+struct ContentSizeReaderPreferenceKey: PreferenceKey {
+  static var defaultValue: CGSize { return CGSize() }
+  static func reduce(value: inout CGSize, nextValue: () -> CGSize) { value = nextValue() }
+}
