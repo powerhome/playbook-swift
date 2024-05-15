f93b3c5b017a1584b426bed8aefa58c5e1612f05
RachelRadford21 <rachelaradford21@gmail.com>
2024-05-15 15:54:18 -0400

    commit to change icon circle catalog

--- Sources/Playbook/Components/IconCircle/IconCircleCatalog.swift
+++ Sources/Playbook/Components/IconCircle/IconCircleCatalog.swift
@@ -13,25 +13,35 @@ public struct IconCircleCatalog: View {
   public var body: some View {
     PBDocStack(title: "Icon Circle") {
       PBDoc(title: "Default") {
-        PBIconCircle(FontAwesome.rocket)
+        defaultView
       }
-
       PBDoc(title: "Size") {
-        VStack(alignment: .leading, spacing: Spacing.small) {
-          let pBIconSizes = [PBIcon.IconSize.small, PBIcon.IconSize.x1, PBIcon.IconSize.large]
-
-          ForEach(pBIconSizes, id: \.self) { size in
-            PBIconCircle(FontAwesome.rocket, size: size)
-          }
-        }
+        sizeView
       }
-
       PBDoc(title: "Color") {
-        VStack(spacing: Spacing.small) {
-          ForEach(Color.DataColor.allCases, id: \.self) { color in
-            PBIconCircle(FontAwesome.rocket, size: .small, color: Color.data(color))
-          }
-        }
+        colorView
+      }
+    }
+  }
+}
+
+extension IconCircleCatalog {
+  var defaultView: some View {
+    PBIconCircle(FontAwesome.rocket)
+  }
+  var sizeView: some View {
+    VStack(alignment: .leading, spacing: Spacing.small) {
+      let pBIconSizes = [PBIcon.IconSize.small, PBIcon.IconSize.x1, PBIcon.IconSize.large]
+
+      ForEach(pBIconSizes, id: \.self) { size in
+        PBIconCircle(FontAwesome.rocket, size: size)
+      }
+    }
+  }
+  var colorView: some View {
+    VStack(spacing: Spacing.small) {
+      ForEach(Color.DataColor.allCases, id: \.self) { color in
+        PBIconCircle(FontAwesome.rocket, size: .small, color: Color.data(color))
       }
     }
   }
