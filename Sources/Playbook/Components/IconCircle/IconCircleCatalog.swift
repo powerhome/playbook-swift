b32385c545af351e8121d57cce9439dfd0d23c38
RachelRadford21 <rachelaradford21@gmail.com>
2024-05-15 15:50:33 -0400

    dark mode grid highlght home icon

--- Sources/Playbook/Components/IconCircle/IconCircleCatalog.swift
+++ Sources/Playbook/Components/IconCircle/IconCircleCatalog.swift
@@ -11,33 +11,28 @@ import SwiftUI
 
 public struct IconCircleCatalog: View {
   public var body: some View {
-    ScrollView {
-      VStack(spacing: Spacing.medium) {
-        PBDoc(title: "Default") {
-          PBIconCircle(FontAwesome.rocket)
-        }
+    PBDocStack(title: "Icon Circle") {
+      PBDoc(title: "Default") {
+        PBIconCircle(FontAwesome.rocket)
+      }
 
-        PBDoc(title: "Size") {
-          VStack(alignment: .leading, spacing: Spacing.small) {
-            let pBIconSizes = [PBIcon.IconSize.small, PBIcon.IconSize.x1, PBIcon.IconSize.large]
+      PBDoc(title: "Size") {
+        VStack(alignment: .leading, spacing: Spacing.small) {
+          let pBIconSizes = [PBIcon.IconSize.small, PBIcon.IconSize.x1, PBIcon.IconSize.large]
 
-            ForEach(pBIconSizes, id: \.self) { size in
-              PBIconCircle(FontAwesome.rocket, size: size)
-            }
-          }
-        }
-
-        PBDoc(title: "Color") {
-          VStack(spacing: Spacing.small) {
-            ForEach(Color.DataColor.allCases, id: \.self) { color in
-              PBIconCircle(FontAwesome.rocket, size: .small, color: Color.data(color))
-            }
+          ForEach(pBIconSizes, id: \.self) { size in
+            PBIconCircle(FontAwesome.rocket, size: size)
           }
         }
       }
-      .padding(Spacing.medium)
+
+      PBDoc(title: "Color") {
+        VStack(spacing: Spacing.small) {
+          ForEach(Color.DataColor.allCases, id: \.self) { color in
+            PBIconCircle(FontAwesome.rocket, size: .small, color: Color.data(color))
+          }
+        }
+      }
     }
-    .background(Color.background(Color.BackgroundColor.light))
-    .navigationTitle("Icon Circle")
   }
 }
