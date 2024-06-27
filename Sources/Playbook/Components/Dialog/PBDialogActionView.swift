6d12a72a5e47e009a1e4d7139a774df673d4faba
Ísis <isis.silva@powerhrg.com>
2024-06-27 09:50:58 -0400

    [PBIOS-420] Expose button variants on dialog kit (#395)
    **What does this PR do?** A clear and concise description with your
    runway ticket url.
    Fix Dialog kit to expose PBButtons setups. 
    
    **Screenshots:** Screenshots to visualize your addition/change
    
    
    https://github.com/powerhome/playbook-swift/assets/60269827/f868828d-2940-4459-8790-3e6b51708114
    
    
    
    **How to test?** Steps to confirm the desired behavior:
    1. Go to '...'
    2. Click on '....'
    3. Scroll down to '....'
    4. See addition/change
    
    ### Checklist
    - [ ] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [ ] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [ ] **TESTING** - Have you tested your story?
    
    ---------
    
    Co-authored-by: RachelRadford21 <rachelaradford21@gmail.com>

--- Sources/Playbook/Components/Dialog/PBDialogActionView.swift
+++ Sources/Playbook/Components/Dialog/PBDialogActionView.swift
@@ -10,75 +10,83 @@
 import SwiftUI
 
 struct PBDialogActionView: View {
-  let isStacked: Bool
-  let confirmButton: (String, (() -> Void))?
-  let cancelButton: (String, (() -> Void)?)?
-  let variant: DialogVariant?
-
-  public init(
-    isStacked: Bool = false,
-    confirmButton: (String, (() -> Void))? = nil,
-    cancelButton: (String, (() -> Void)?)? = nil,
-    variant: DialogVariant? = nil
-  ) {
-    self.isStacked = isStacked
-    self.confirmButton = confirmButton
-    self.cancelButton = cancelButton
-    self.variant = variant
-  }
-
-  var body: some View {
-    AdaptiveStack(isStacked: isStacked) {
-      if let confirmButton = confirmButton {
-        PBButton(
-          fullWidth: true,
-          title: confirmButton.0,
-          action: confirmButton.1
-        ).padding(.bottom, Spacing.xxSmall)
-      }
-
-      if let cancelButton = cancelButton {
-        if !isStacked {
-          Spacer()
-        }
-
-        PBButton(
-          fullWidth: true, variant: (isStacked || variant != .default ? .secondary : .link),
-          title: cancelButton.0,
-          action: cancelButton.1 ?? {}
-        )
-        .padding(.top, isStacked ? 5 : 0)
-      }
+    let isStacked: Bool
+    let confirmButton: (PBButton, (() -> Void)?)?
+    let cancelButton: (PBButton, (() -> Void)?)?
+    let variant: DialogVariant?
+    
+    public init(
+        isStacked: Bool = false,
+        confirmButton: (PBButton, (() -> Void)?)? = nil,
+        cancelButton: (PBButton, (() -> Void)?)? = nil,
+        variant: DialogVariant? = nil
+    ) {
+        self.isStacked = isStacked
+        self.confirmButton = confirmButton
+        self.cancelButton = cancelButton
+        self.variant = variant
     }
-    .frame(maxWidth: .infinity)
-    .padding(.bottom, Spacing.small)
-    .padding(.leading, Spacing.small)
-    .padding(.trailing, Spacing.small)
-   
-  }
+    
+    var body: some View {
+        AdaptiveStack(isStacked: isStacked) {
+            if let confirmButton = confirmButton {
+                PBButton(
+                    fullWidth: confirmButton.0.fullWidth,
+                    variant: confirmButton.0.variant,
+                    size: confirmButton.0.size,
+                    title: confirmButton.0.title,
+                    icon: confirmButton.0.icon,
+                    iconPosition: confirmButton.0.iconPosition,
+                    isLoading: confirmButton.0.$isLoading,
+                    action: confirmButton.1
+                )
+                .padding(.bottom, Spacing.xxSmall)
+            }
+            
+            if let cancelButton = cancelButton {
+                if !isStacked {
+                    Spacer()
+                }
+                
+                PBButton(
+                    fullWidth: cancelButton.0.fullWidth,
+                    variant: cancelButton.0.variant,
+                    size: cancelButton.0.size,
+                    title: cancelButton.0.title,
+                    icon: cancelButton.0.icon,
+                    iconPosition: cancelButton.0.iconPosition,
+                    isLoading: cancelButton.0.$isLoading,
+                    action: cancelButton.1
+                )
+                .padding(.top, isStacked ? 5 : 0)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding(.bottom, Spacing.small)
+        .padding(.leading, Spacing.small)
+        .padding(.trailing, Spacing.small)
+    }
 }
 
-struct PBDialogActionView_Previews: PreviewProvider {
-  static var previews: some View {
+#Preview {
     registerFonts()
-
+    
     return List {
-      PBDialogActionView(
-        confirmButton: ("Okay", {}),
-        cancelButton: ("Cancel", {})
-      )
-      .listRowSeparator(.hidden)
-
-      PBDialogActionView(
-        isStacked: true,
-        confirmButton: ("Yes, Action", {}),
-        cancelButton: ("No, Cancel", {})
-      )
-      .listRowSeparator(.hidden)
-
-      PBDialogActionView(
-        confirmButton: ("Okay", {})
-      )
+        PBDialogActionView(
+            confirmButton: (PBButton(title: "Okay") { print("ola") }, nil),
+            cancelButton: (PBButton(title: "Cancel"), { print("cancel") })
+        )
+        .listRowSeparator(.hidden)
+        
+        PBDialogActionView(
+            isStacked: true,
+            confirmButton: (PBButton(title: "Okay"), nil),
+            cancelButton: (PBButton(title: "No, Cancel"), {})
+        )
+        .listRowSeparator(.hidden)
+        
+        PBDialogActionView(
+            confirmButton: (PBButton(title: "Okay"), nil)
+        )
     }
-  }
 }
