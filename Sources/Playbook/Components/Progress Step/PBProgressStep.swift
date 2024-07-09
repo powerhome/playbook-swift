bd3556274e50d21fef79c498fa84aa124d23eedc
RachelRadford21 <rachelaradford21@gmail.com>
2024-07-09 12:08:00 -0400

    progress commit

--- Sources/Playbook/Components/Progress Step/PBProgressStep.swift
+++ Sources/Playbook/Components/Progress Step/PBProgressStep.swift
@@ -13,35 +13,35 @@ public struct PBProgressStep: View {
   let icon: FontAwesome
   let iconSize: PBIcon.IconSize
   let variant: Variant
+  let label: String?
+  @Binding var hasIcon: Bool
   @Binding var isActive: [Bool]
   @Binding var isComplete: [Bool]
-  @Binding var isInactive: [Bool]
-  @Binding var isBarComplete: Bool
-  let hasIcon: Bool
+  @Binding var active: Int
   
   public init(
     icon: FontAwesome = .check,
     iconSize: PBIcon.IconSize = .xSmall,
     variant: Variant = .horizontal,
+    label: String? = nil,
+    active: Binding<Int> = .constant(2),
+    hasIcon: Binding<Bool> = .constant(true),
     isActive: Binding<[Bool]> = .constant([false, true, false]),
-    isComplete: Binding<[Bool]> = .constant([true, false, false]),
-    isInactive: Binding<[Bool]> = .constant([false, false, true]),
-    isBarComplete: Binding<Bool> = .constant(false),
-    hasIcon: Bool = false
+    isComplete: Binding<[Bool]> = .constant([true, false, false])
   ) {
     self.icon = icon
     self.iconSize = iconSize
     self.variant = variant
+    self.label = label
+    self._active = active
+    self._hasIcon = hasIcon
     self._isActive = isActive
     self._isComplete = isComplete
-    self._isInactive = isInactive
-    self._isBarComplete = isBarComplete
-    self.hasIcon = hasIcon
   }
-    public var body: some View {
-      progressStepView()
-      
-    }
+  
+  public var body: some View {
+    progressStepView()
+  }
 }
 
 public extension PBProgressStep {
@@ -49,41 +49,54 @@ public extension PBProgressStep {
     case vertical, horizontal
   }
   func progressStepView() -> some View {
-      HStack(spacing: 0) {
-        ForEach(isActive.indices, id: \.self) { index in
+    HStack(spacing: 0) {
+      ForEach(isActive.indices, id: \.self) { index in
+        VStack {
           iconView(
             isActive: isActive[index],
-            isComplete: isComplete[index],
-            isInactive: isInactive[index]
+            isComplete: isComplete[index]
           )
-          if index < isActive.count - 1 {
-            progressView(isComplete: isComplete[index])
-          }
+          labelView
+        }
+        
+        if index < isActive.count - 1 {
+          progressView(isComplete: isComplete[index])
         }
       }
     }
-  func iconView(isActive: Bool, isComplete: Bool, isInactive: Bool) -> some View {
+  }
+  func iconView(isActive: Bool, isComplete: Bool) -> some View {
+    VStack {
       Circle()
-        .strokeBorder(isActive ? Color.pbPrimary : Color.white, lineWidth: 2.5)
+        .strokeBorder(isActive ? Color.pbPrimary : Color.white, lineWidth: 2)
         .background(Circle().fill(isComplete ? Color.pbPrimary : isActive ? Color.white : Color.border))
-        .frame(width: 20, height: 20)
+        .frame(width: isActive ? 15 : 20, height: isActive ? 15 : 20)
         .overlay {
           PBIcon(icon, size: iconSize)
             .foregroundStyle(isComplete || isActive ? Color.white : Color.border)
-            .opacity(isComplete || hasIcon ? 1 : 0)
+            .opacity(isComplete && hasIcon ? 1 : 0)
         }
-        .padding(.horizontal, isActive ? 3 : 0)
+        .padding(.horizontal, isActive ? 2 : 0)
     }
-
+  }
   func progressView(isComplete: Bool) -> some View {
-      HStack(spacing: 0) {
-        RoundedRectangle(cornerRadius: 2)
-          .frame(width: 100, height: 5)
-          .foregroundStyle(isComplete ? Color.pbPrimary : Color.border)
+    HStack(spacing: 0) {
+      RoundedRectangle(cornerRadius: 2)
+        .frame(width: 125, height: 5)
+        .foregroundStyle(isComplete ? Color.pbPrimary : Color.border)
+    }
+  }
+  var labelView: some View {
+    HStack {
+      if let label = label {
+        Text("\(label)")
+          .pbFont(.subcaption, variant: .bold, color: .text(.default))
       }
     }
+  }
 }
+
 #Preview {
   registerFonts()
-   return PBProgressStep()
+  return PBProgressStep()
 }
--- Sources/Playbook/Components/Progress Step/ProgressStepCatalog.swift
+++ Sources/Playbook/Components/Progress Step/ProgressStepCatalog.swift
@@ -10,31 +10,62 @@
 import SwiftUI
 
 public struct ProgressStepCatalog: View {
+  @State private var hasIcon: Bool = false
   @State private var isActive = [false, true, false]
-    @State private var isComplete = [true, false, false]
-    @State private var isInactive = [false, false, true]
-    public var body: some View {
-      PBDocStack(title: "Progress Step", spacing: Spacing.medium) {
-        PBDoc(title: "Default") {
-          defaultView
-        }
+  @State private var isComplete = [true, false, false]
+  @State private var isActive1 = [false, true, false]
+  @State private var isComplete1 = [true, false, false]
+  public var body: some View {
+    PBDocStack(title: "Progress Step", spacing: Spacing.medium) {
+      PBDoc(title: "Default") {
+        defaultView
       }
     }
+  }
 }
 
 extension ProgressStepCatalog {
   var defaultView: some View {
-    VStack(alignment: .leading, spacing: Spacing.small) {
-        PBProgressStep(
-          isActive: $isActive,
-          isComplete: $isComplete,
-          isInactive: $isInactive
-        )
+    VStack(alignment: .leading, spacing: Spacing.medium) {
+      PBProgressStep(
+        isActive: $isActive,
+        isComplete: $isComplete
+  
+      )
+      PBProgressStep(
+        label: "Step",
+        hasIcon: $hasIcon,
+        isActive: $isActive1,
+        isComplete: $isComplete1
       
+      )
+//      Button("Submit") {
+//
+//        if let currentActiveIndex = isActive.firstIndex(where: { $0 }) {
+//            isComplete[currentActiveIndex] = true
+//            isActive[currentActiveIndex] = false
+//            if currentActiveIndex + 1 < isActive.count {
+//              isActive[currentActiveIndex + 1] = true
+//            }
+//       }
+//        
+//        
+//
+//        else {
+//           
+//          if let currentActiveIndex = isComplete.firstIndex(where: { $0 == false }) {
+//                        isComplete[currentActiveIndex] = true
+//                        isActive[currentActiveIndex] = false
+//                        if currentActiveIndex + 1 < isActive.count {
+//                          isActive[currentActiveIndex + 1] = true
+//                        }
+//                   }
+//            }
+//          }
     }
   }
 }
 #Preview {
   registerFonts()
-   return ProgressStepCatalog()
+  return ProgressStepCatalog()
 }
