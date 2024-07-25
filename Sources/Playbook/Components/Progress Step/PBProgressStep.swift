1898ea31b83b089f0f0e07a9616cdb0a57c30cac
RachelRadford21 <rachelaradford21@gmail.com>
2024-07-25 10:34:01 -0400

    progress tracker wip

--- Sources/Playbook/Components/Progress Step/PBProgressStep.swift
+++ Sources/Playbook/Components/Progress Step/PBProgressStep.swift
@@ -35,8 +35,8 @@ public struct PBProgressStep: View {
     variant: Variant = .horizontal,
     customLabel: [String] = [""],
     overlap: CGFloat = 0,
-    progress: Binding<Int> = .constant(1),
-    active: Binding<Int> = .constant(1)
+    progress: Binding<Int> = .constant(0),
+    active: Binding<Int> = .constant(0)
   ) {
     self.hasIcon = hasIcon
     self.icon = icon
@@ -83,8 +83,11 @@ public extension PBProgressStep {
   
   var progressStepsView: some View {
     ForEach(1...steps, id: \.hashValue) { step in
-      circleIconView(isActive: progress == 0  || step != progress + 1 ? false : true, isComplete: progress >= step ? true : false)
-        .globalPosition(alignment: variant == .horizontal ? .bottom : .leading, bottom: variant == .horizontal ? -30 : 0, isCard: false) {
+      circleIconView(
+        isActive: progress == 0  || step != progress + 1 ? false : true,
+        isComplete: progress >= step ? true : false
+      )
+      .globalPosition(alignment: variant == .horizontal ? .bottom : .leading, bottom: variant == .horizontal ? -30 : 0, isCard: false) {
           labelView(index: Int(step) - 1)
             .padding(.leading, variant == .vertical ? 25 : 0)
             .padding(.trailing, 0)
@@ -93,7 +96,7 @@ public extension PBProgressStep {
         PBProgressPill(
           steps: 1,
           pillWidth: variant == .horizontal ? frameReader(in: { _ in}) as? CGFloat : 4,
-          pillHeight: variant == .horizontal ? 4 : 40,
+          pillHeight: pillHeight,
           progressBarColorTrue: step <= progress ? Color.pbPrimary : Color.text(.lighter),
           progressBarColorFalse:  step > progress ? Color.text(.lighter) : Color.pbPrimary,
           cornerRadius: 1
@@ -101,7 +104,6 @@ public extension PBProgressStep {
       }
     }
   }
-  
   func circleIconView(isActive: Bool, isComplete: Bool) -> some View {
     ZStack {
       circleIcon(
@@ -158,72 +160,23 @@ public extension PBProgressStep {
 extension PBProgressStep {
 
   var trackerView: some View {
-    GeometryReader { geo in
-      ZStack(alignment: .leading) {
-        PBProgressPill(
-          steps: 1,
-          pillWidth: geo.size.width,
-          pillHeight: 28,
-          progressBarColorTrue: progress < 0 ? .pbPrimary : .text(.lighter).opacity(0.5)
-        )
-        .clipShape(RoundedRectangle(cornerRadius: 15))
-        PBProgressPill(
-          active: $active,
-          steps: 1,
-          pillWidth: progress > steps ? geo.size.width : (geo.size.width / CGFloat(steps) * CGFloat(progress)),
-          pillHeight: 28
-        )
-        .clipShape(RoundedRectangle(cornerRadius: 15))
-        HStack(spacing: Spacing.none) {
-            ForEach(1...steps, id: \.hashValue) { step in
-//              if step >= 1 {
-              //  Spacer(minLength: 2)
-//              }
-              circleIcon(
-                icon: icon,
-                iconSize: iconSize,
-                strokeColor: .clear,
-                lineWidth: 2,
-                background: progress >= step ? .black : progress == step ? .border : .text(.lighter),
-                circleWidth: 20,
-                circleHeight: 20,
-                iconColor: step <= progress ? .white : step == 1 ? .clear : step == progress + 1 ? .pbPrimary : .clear,
-                offsetX: 0,
-                offsetY: 0,
-                opacity: 1
-              )
-            }
-            .frame(width: geo.size.width / CGFloat(steps) + 23)
-            .padding(.leading, -19)
-            .padding(.trailing, -1)
+      GeometryReader { geo in
+        HStack {
+          PBProgressStep(
+            hasIcon: true,
+            icon: .check,
+            iconSize: .small,
+            pillHeight: 28,
+            steps: steps,
+            progress: $progress,
+            active: $active
+          )
+          .padding(.horizontal, 5)
         }
+        .background(Color.pbPrimary)
+        .clipShape(RoundedRectangle(cornerRadius: 15))
       }
-    }
   }
-//  var trackerSpacing: CGFloat {
-//    switch steps {
-//    case 0, 1: return 0
-//    case 2: return Spacing.xLarge + 46
-//    case 3: return Spacing.xLarge
-//    case 4: return Spacing.medium + 0.5
-//    case 5: return Spacing.small + 0.5
-//    case 6: return Spacing.xSmall + 4
-//    case 7: return Spacing.xxSmall + 4
-//    case 8: return Spacing.xxSmall
-//    default:
-//      return Spacing.none
-//    }
-//  }
-//  var progressSpacing: CGFloat {
-//    switch steps {
-//    case 0, 1, 2: return 0
-//    case 3: return 65
-//    case 4: return 45
-//    case 5: return 40
-//    case 6: return 40
-//    default: return 0
-//    }
-//  }
 }
 #Preview {
   registerFonts()
--- Sources/Playbook/Components/Progress Step/ProgressStepCatalog.swift
+++ Sources/Playbook/Components/Progress Step/ProgressStepCatalog.swift
@@ -93,7 +93,7 @@ extension ProgressStepCatalog {
         //          progress: $trackerProgress
         //        )
         PBProgressStep(
-          steps: 6,
+          steps: 3,
           variant: .tracker,
           progress: $trackerProgress1,
           active: $active
