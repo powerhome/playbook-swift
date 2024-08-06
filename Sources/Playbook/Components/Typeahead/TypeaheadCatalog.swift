e3184e74716130c10028d6e6bf243c24de08a028
isis.silva <isisms2602@gmail.com>
2024-08-06 14:05:11 -0400

    polishes

--- Sources/Playbook/Components/Typeahead/TypeaheadCatalog.swift
+++ Sources/Playbook/Components/Typeahead/TypeaheadCatalog.swift
@@ -18,28 +18,21 @@ public struct TypeaheadCatalog: View {
     @State private var searchTextDebounce: String = ""
     @State private var didTapOutside: Bool? = false
     @State private var isPresented1: Bool = false
-    
     @State private var presentDialog: Bool = false
-    @State private var presentDialog1: Bool = false
     @State private var isLoading: Bool = false
-    
     @State private var assetsUser = Mocks.multipleUsersDictionary
-    @FocusState var isFocused
-    
-    var popoverManager = PopoverManager()
     @FocusState var isFocused1
     @FocusState var isFocused2
-    
-    @State private var toastView: PBToast?
-    @State private var position: PBToast.Position = .top
+
+    var popoverManager = PopoverManager()
     
     public var body: some View {
         PBDocStack(title: "Typeahead") {
             PBDoc(title: "Default", spacing: Spacing.small) { colors }
             PBDoc(title: "With Pills", spacing: Spacing.small) { users }
-            //      PBDoc(title: "Debounce", spacing: Spacing.small) { debounce }
-            PBDoc(title: "Custom Dialog") { customDialog }
+            #if os(macOS)
             PBDoc(title: "Dialog") { dialog }
+            #endif
         }
         .onTapGesture {
             isFocused1 = false
@@ -47,8 +40,6 @@ public struct TypeaheadCatalog: View {
         }
         .popoverHandler(id: 1)
         .popoverHandler(id: 2)
-//        .popoverHandler(id: 3)
-//        .popoverHandler(id: )
     }
 }
 
@@ -79,33 +70,18 @@ extension TypeaheadCatalog {
     func closeToast() {
         presentDialog = false
     }
-    
-    var customDialog: some View {
-        PBButton(title: "Simple") {
-            DialogCatalog.disableAnimation()
-            presentDialog1.toggle()
-        }
-        .sheet(isPresented: $presentDialog1) {
-            CustomDialogView(isPresented: $presentDialog1)
-                .popoverHandler(id: 3)
-#if os(macOS)
-                .frame(minWidth: 500, minHeight: 390)
-#endif
-        }
-    }
-    
+
     var dialog: some View {
         PBButton(title: "Simple") {
             DialogCatalog.disableAnimation()
             presentDialog.toggle()
         }
-        .sheet(isPresented: $presentDialog) {
+        .presentationMode(isPresented: $presentDialog) {
             DialogView(isPresented: $presentDialog)
                 .popoverHandler(id: 4)
-#if os(macOS)
+                #if os(macOS)
                 .frame(minWidth: 500, minHeight: 390)
-#endif
-            
+                #endif
         }
     }
     
@@ -145,42 +121,6 @@ extension TypeaheadCatalog {
         }
     }
     
-    struct CustomDialogView: View {
-        @Binding var isPresented: Bool
-        @State private var isLoading: Bool = false
-        @State private var searchTextUsers: String = ""
-        @State private var assetsUsers = Mocks.multipleUsersDictionary
-        @FocusState var isFocused
-        
-        var body: some View {
-            VStack {
-                HStack {
-                    Spacer()
-                    PBButton(icon: PBIcon.fontAwesome(.times)) {
-                        isPresented = false
-                    }
-                }
-                PBTypeahead(
-                    id: 3,
-                    title: "Users",
-                    placeholder: "type the name of a user",
-                    searchText: $searchTextUsers,
-                    selection: .multiple(variant: .pill),
-                    options: assetsUsers,
-                    dropdownMaxHeight: 300,
-                    isFocused: $isFocused
-                ) { options in
-                    print("Selected options \(options)")
-                }
-                Spacer()
-            }
-            .padding(Spacing.medium)
-            .background(Color.white.opacity(0.01))
-            .onTapGesture {
-                isFocused = false
-            }
-        }
-    }
     //  var debounce: some View {
     //    VStack(spacing: Spacing.small) {
     //      PBTypeahead(
