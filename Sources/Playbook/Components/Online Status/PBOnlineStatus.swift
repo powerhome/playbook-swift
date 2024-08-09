2fb9a85343fb6cc3db4996caf4e83a37fc6d7dbd
Rachel Radford <54749071+RachelRadford21@users.noreply.github.com>
2024-08-09 09:07:58 -0400

    [PBIOS-468] Online Status Kit (#422)
    **What does this PR do?** 
    [PBIOS-468] Online Status Kit
    
    **Screenshots:** 
    ![Simulator Screenshot - iPhone 15 Pro Max - 2024-07-24 at 13 27
    49](https://github.com/user-attachments/assets/7226bb41-6e10-4f42-8249-bfcbc2f052f3)
    
    ### Checklist
    - [x] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [x] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [x] **TESTING** - Have you tested your story?
    
    ---------
    
    Co-authored-by: Ísis <isis.silva@powerhrg.com>

---
+++ Sources/Playbook/Components/Online Status/PBOnlineStatus.swift
@@ -0,0 +1,76 @@
+//
+//  Playbook Swift Design System
+//
+//  Copyright © 2024 Power Home Remodeling Group
+//  This software is distributed under the ISC License
+//
+//  PBOnlineStatus.swift
+//
+
+import SwiftUI
+
+public struct PBOnlineStatus: View {
+    let color: Color
+    let size: Size
+    let borderColor: Color?
+  let variant: Variant
+    public init(
+        color: Color = .status(.neutral),
+        size: Size = .small,
+        borderColor: Color? = .white,
+        variant: Variant = .border
+    ) {
+        self.borderColor = borderColor
+        self.color = color
+        self.size = size
+      self.variant = variant
+    }
+  public var body: some View {
+    statusView
+  }
+}
+
+public extension PBOnlineStatus {
+  enum Variant {
+    case border, borderless
+  }
+  private var hasBorder: Bool {
+    return borderColor != nil
+  }
+  
+  private var statusView: some View {
+    Circle()
+      .stroke(_borderColor, lineWidth: borderWidth)
+      .background(Circle().fill(color))
+      .frame(width: _size, height: _size)
+  }
+  
+  var _borderColor: Color {
+    switch variant {
+    case .border: return borderColor ?? .clear
+    case .borderless: return .clear
+    }
+  }
+  var borderWidth: CGFloat {
+    switch variant {
+    case .border: return 2
+    case .borderless: return 0
+    }
+  }
+  private var _size: CGFloat {
+    switch size {
+    case .small: return hasBorder ? 8 : 6
+    case .medium: return hasBorder ? 10 : 8
+    case .large: return hasBorder ? 12 : 10
+    }
+  }
+  
+  enum Size {
+    case small, medium, large
+  }
+}
+
+#Preview {
+    registerFonts()
+    return PBOnlineStatus()
+}
