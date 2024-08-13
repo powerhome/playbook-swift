55e7104b1ad7023b7b2e523cec22d7098ad784f4
RachelRadford21 <rachelaradford21@gmail.com>
2024-08-13 15:53:26 -0400

    commit

--- Sources/Playbook/Components/Avatar/PBAvatar.swift
+++ Sources/Playbook/Components/Avatar/PBAvatar.swift
@@ -159,8 +159,8 @@ public extension PBAvatar {
   }
   var avatarBorderColor: Color {
     switch colorScheme {
-    case .light: return Color.white
-    case .dark: return Color.background(.dark)
+    case .light: return Color.BorderColor.background(.light)
+    case .dark: return Color.BorderColor.background(.dark)
     default:
       return Color.white
       
