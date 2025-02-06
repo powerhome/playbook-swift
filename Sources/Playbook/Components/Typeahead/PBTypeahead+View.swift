430071e58608ab7dfb9265b2aa3dc711c02e3061
isis.silva <isisms2602@gmail.com>
2025-02-06 13:32:46 -0500

    fix padding

--- Sources/Playbook/Components/Typeahead/PBTypeahead+View.swift
+++ Sources/Playbook/Components/Typeahead/PBTypeahead+View.swift
@@ -76,9 +76,9 @@ public extension PBTypeahead {
                     } else {
                         Text(option.text ?? option.id)
                             .pbFont(.body, color: listTextolor(index))
-                            .padding(.vertical, Spacing.xSmall + 4)
                     }
                 }
+                .padding(.vertical, Spacing.xSmall)
                 .padding(.horizontal, Spacing.xSmall + 4)
                 .frame(maxWidth: .infinity, alignment: .leading)
                 .background(listBackgroundColor(index))
