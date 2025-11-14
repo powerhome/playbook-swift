10c7d4564720b8d3ad6dc5b2ca5674b1c23c957f
Ísis <isis.silva@powerhrg.com>
2025-11-14 09:04:42 -0500

    [RANGR-1241] Fix vertical section separator (#553)
    **What does this PR do?** 
    Made Vertical Section Separator vertical again
    
    Task on Runway
    [RANGR-1241](https://nitro.powerhrg.com/runway/backlog_items/RANGR-1241).
    
    **Screenshots:** Screenshots to visualize your addition/change
    <img width="585" height="163" alt="Screenshot 2025-11-11 at 1 47 45 PM"
    src="https://github.com/user-attachments/assets/a29ede99-4aff-47ec-ba2d-51e9edfa1c96"
    />
    
    3. Scroll down to '....'
    4. See addition/change
    
    ### Checklist
    - [ ] **LABELS** - Add a label: `breaking`, `bug`, `improvement`,
    `documentation`, or `enhancement`. See
    [Labels](https://github.com/powerhome/playbook-apple/labels) for
    descriptions.
    - [ ] **RELEASES** - Add the appropriate label: `Ready for Testing` /
    `Ready for Release`
    - [ ] **TESTING** - Have you tested your story?
    
    ---------
    
    Co-authored-by: RachelRadford21 <rachelaradford21@gmail.com>

--- Sources/Playbook/Components/Section Separator/PBLine.swift
+++ Sources/Playbook/Components/Section Separator/PBLine.swift
@@ -10,10 +10,16 @@
 import SwiftUI
 
 public struct PBLine: Shape {
+  let orientation: Orientation
+
   public func path(in rect: CGRect) -> Path {
     var path = Path()
     path.move(to: CGPoint(x: 0, y: 0))
-    path.addLine(to: CGPoint(x: rect.width, y: 0))
+      if orientation == .horizontal {
+          path.addLine(to: CGPoint(x: rect.width, y: 0))
+      } else {
+          path.addLine(to: CGPoint(x: 0, y: rect.height))
+      }
     return path
   }
 }
