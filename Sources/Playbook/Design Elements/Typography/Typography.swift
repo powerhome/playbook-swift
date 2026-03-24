d6a6bf4e0822dfdc7804ef2f8a99b6b91b58f9da
Welington Ramos <welington.ramos@powerhrg.com>
2026-03-24 10:54:12 -0300

    [ROGUE-2478] fix text padding space

--- Sources/Playbook/Design Elements/Typography/Typography.swift
+++ Sources/Playbook/Design Elements/Typography/Typography.swift
@@ -57,10 +57,10 @@ private extension Typography {
     case .title1: return 4.6
     case .title2: return -0.7
     case .title3: return 2.8
-    case .title4: return 1.6
+    case .title4: return 0
     case .body: return 3.2
     case .detail: return 2.8
-    case .caption, .subcaption: return 3
+    case .caption, .subcaption: return 0
     case .largeCaption: return 5
     case .messageTitle: return 1.4
     case .messageBody: return 3
